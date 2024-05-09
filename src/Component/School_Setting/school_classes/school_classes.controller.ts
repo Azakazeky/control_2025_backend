40fda148d168d798ccb0c1e5433a0dbbbea4792f
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 10:04:29 +0300

    ✨ Add JwtAuthGuard and Roles decorators to SchoolClassesController
    Add JwtAuthGuard to secure controller endpoints and Roles decorator to specify
    permissions for certain actions in SchoolClassesController. This ensures
    authorization and role-based access control for the SchoolClasses endpoints.

--- src/Component/School_Setting/school_classes/school_classes.controller.ts
+++ src/Component/School_Setting/school_classes/school_classes.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SchoolClassesService } from './school_classes.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
+import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
+import Role from 'src/Common/Guard/role.enum';
+import { Roles } from 'src/Common/Guard/roles.decorator';
 import { CreateSchoolClassDto } from './dto/create-school_class.dto';
 import { UpdateSchoolClassDto } from './dto/update-school_class.dto';
-
-@Controller('school-classes')
-export class SchoolClassesController {
-  constructor(private readonly schoolClassesService: SchoolClassesService) {}
+import { SchoolClassesService } from './school_classes.service';
+@UseGuards( JwtAuthGuard )
+@ApiTags( "School-Classes" )
+@UseGuards( PrismaExceptionFilter )
+@Controller( 'school-classes' )
+export class SchoolClassesController
+{
+  constructor ( private readonly schoolClassesService: SchoolClassesService ) { }
+  @Roles( Role.SuperAdmin )
 
   @Post()
-  create(@Body() createSchoolClassDto: CreateSchoolClassDto) {
-    return this.schoolClassesService.create(createSchoolClassDto);
+  create ( @Body() createSchoolClassDto: CreateSchoolClassDto )
+  {
+    return this.schoolClassesService.create( createSchoolClassDto );
   }
 
   @Get()
-  findAll() {
+  findAll ()
+  {
     return this.schoolClassesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.schoolClassesService.findOne(+id);
+  @Get( ':id' )
+  findOne ( @Param( 'id' ) id: string )
+  {
+    return this.schoolClassesService.findOne( +id );
   }
+  @Roles( Role.SuperAdmin )
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSchoolClassDto: UpdateSchoolClassDto) {
-    return this.schoolClassesService.update(+id, updateSchoolClassDto);
+  @Patch( ':id' )
+  update ( @Param( 'id' ) id: string, @Body() updateSchoolClassDto: UpdateSchoolClassDto )
+  {
+    return this.schoolClassesService.update( +id, updateSchoolClassDto );
   }
+  @Roles( Role.SuperAdmin )
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.schoolClassesService.remove(+id);
+  @Delete( ':id' )
+  remove ( @Param( 'id' ) id: string )
+  {
+    return this.schoolClassesService.remove( +id );
   }
 }
