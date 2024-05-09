39f3a9804bd4c499add1e80d54207135541c517a
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 09:29:44 +0300

    ✨ Improve CohortController with authentication and error handling
    Added JwtAuthGuard and PrismaExceptionFilter for authentication and error
    handling in CohortController. Also added ApiTags for better organization of
    swagger documentation.

--- src/Component/School_Setting/cohort/cohort.controller.ts
+++ src/Component/School_Setting/cohort/cohort.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
+import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
 import Role from 'src/Common/Guard/role.enum';
 import { Roles } from 'src/Common/Guard/roles.decorator';
 import { CohortService } from './cohort.service';
 import { CreateCohortDto } from './dto/create-cohort.dto';
 import { UpdateCohortDto } from './dto/update-cohort.dto';
 
+@UseGuards( JwtAuthGuard )
+@ApiTags( "cohort" )
+@UseGuards( PrismaExceptionFilter )
 @Controller( 'cohort' )
 export class CohortController
 {
