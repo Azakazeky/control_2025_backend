baa5909938cd8ca4dc7969e4c9bb9e61d6c1131c
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 11:16:19 +0300

    ✨ Add CRUD functionalities to Student Entity
    Add create, read, update, and delete functionalities to the Student entity to manage student data efficiently. This includes API endpoints for creating a student, retrieving all students, updating student details, and deleting a student. Additionally, create data transfer object (DTO) for creating a student with necessary validations. Update student service to interact with the Prisma database service for CRUD operations.

--- src/Component/student/dto/create-student.dto.ts
+++ src/Component/student/dto/create-student.dto.ts
@@ -1,1 +1,33 @@
-export class CreateStudentDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateStudentDto
+{
+    @ApiProperty()
+    @IsNumber()
+    Grades_ID: number;
+
+    @ApiProperty()
+    @IsNumber()
+    Schools_ID: number;
+
+    @ApiProperty()
+    @IsNumber()
+    Cohort_ID: number;
+
+    @ApiProperty()
+    @IsNumber()
+    School_Class_ID: number;
+
+    @ApiProperty()
+    @IsString()
+    First_Name: string;
+
+    @ApiProperty()
+    @IsString()
+    Second_Name: string;
+
+    @ApiProperty()
+    @IsString()
+    Third_Name: string;
+}
