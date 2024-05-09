53d52a1d634331674682c7bde20478412c88e102
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 10:51:25 +0300

    ✨ Refactor subjects endpoints and service
    Refactor subjects endpoints and service to include Swagger annotations, customize
    guards, and improve Prisma integration. Update DTOs to include validation.

--- src/Component/School_Setting/subjects/subjects.service.ts
+++ src/Component/School_Setting/subjects/subjects.service.ts
@@ -1,26 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/Common/Db/prisma.service';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
 
 @Injectable()
-export class SubjectsService {
-  create(createSubjectDto: CreateSubjectDto) {
-    return 'This action adds a new subject';
+export class SubjectsService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
+
+  async create ( createSubjecteDto: CreateSubjectDto )
+  {
+    var result = await this.prismaService.subjects.create( {
+      data: createSubjecteDto
+    } );
+    return result;
   }
 
-  findAll() {
-    return `This action returns all subjects`;
+  async findAll ()
+  {
+    var results = await this.prismaService.subjects.findMany( {
+
+    } );
+
+    return results;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} subject`;
+  // TODO: need to check how to filter
+  // async findAllBy...lId ( schoolId: number )
+  // {
+  //   var results = await this.prismaService.subjects.findMany( {
+  //     where: {
+
+  //     }
+  //   } );
+  //   return results;
+  // }
+
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.subjects.findUnique( {
+      where: {
+        ID: id
+      },
+
+    } );
+    return result;
   }
 
-  update(id: number, updateSubjectDto: UpdateSubjectDto) {
-    return `This action updates a #${id} subject`;
+  async update ( id: number, updateSubjecteDto: UpdateSubjectDto )
+  {
+    var result = await this.prismaService.subjects.update( {
+      where: {
+        ID: id
+      },
+      data: updateSubjecteDto
+    } );
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} subject`;
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.subjects.delete( {
+      where: {
+        ID: id
+      }
+    } );
+    return result;
   }
+
 }
