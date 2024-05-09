6f18ed540922197859bb44c321f8546d58aa5643
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 13:05:33 +0300

    ⚡️ Refactor: Update Users System Structure and Functionality
    Refactored the users controller, dto, and service files to enhance the
    overall functionality and structure of the Users System. Added JWT
    authentication, role-based access control, and improved error handling
    with PrismaExceptionFilter. Updated create, findAll, findOne, update,
    and remove methods for better user handling and data retrieval.

--- src/Users_System/users/users.controller.ts
+++ src/Users_System/users/users.controller.ts
@@ -1,34 +1,52 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
+import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
+import Role from 'src/Common/Guard/role.enum';
+import { Roles } from 'src/Common/Guard/roles.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+@UseGuards( JwtAuthGuard )
+@ApiTags( "Users" )
+@UseGuards( PrismaExceptionFilter )
+@Controller( 'users' )
+export class UsersController
+{
+  constructor ( private readonly usersService: UsersService ) { }
+
+  @Roles( Role.SuperAdmin )
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create ( @Body() createUserDto: CreateUserDto )
+  {
+    return this.usersService.create( createUserDto );
   }
 
   @Get()
-  findAll() {
+  findAll ()
+  {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get( ':id' )
+  findOne ( @Param( 'id' ) id: string )
+  {
+    return this.usersService.findOne( +id );
   }
+  @Roles( Role.SuperAdmin )
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Patch( ':id' )
+  update ( @Param( 'id' ) id: string, @Body() updateUserDto: UpdateUserDto )
+  {
+    return this.usersService.update( +id, updateUserDto );
   }
+  @Roles( Role.SuperAdmin )
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Delete( ':id' )
+  remove ( @Param( 'id' ) id: string )
+  {
+    return this.usersService.remove( +id );
   }
 }
--- src/Users_System/users/users.service.ts
+++ src/Users_System/users/users.service.ts
@@ -1,26 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/Common/Db/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+
 @Injectable()
-export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+export class UsersService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
+
+  async create ( createUserCreateUserDto: CreateUserDto )
+  {
+    var result = await this.prismaService.users.create( {
+      data: createUserCreateUserDto
+    } );
+    return result;
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll ()
+  {
+    var results = await this.prismaService.users.findMany( {
+
+    } );
+
+    return results;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.users.findUnique( {
+      where: {
+        ID: id
+      },
+
+    } );
+    return result;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update ( id: number, updateUserCreateUserDto: UpdateUserDto )
+  {
+    var result = await this.prismaService.users.update( {
+      where: {
+        ID: id
+      },
+      data: updateUserCreateUserDto
+    } );
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.users.delete( {
+      where: {
+        ID: id
+      }
+    } );
+    return result;
   }
 }
