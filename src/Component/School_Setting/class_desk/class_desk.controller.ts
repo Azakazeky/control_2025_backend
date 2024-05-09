f223a3c50758ac4fb7896c2971effd73c143fae0
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 09:16:42 +0300

    feat(school-desk): Add Swagger validation and role-based guards
    - Add Swagger ApiProperty and class-validator validation on CreateClassDeskDto
    - Implement Role-based guards within ClassDeskController for endpoint security
    - Utilize JwtAuthGuard and Roles decorator for controlling user access levels

--- src/Component/School_Setting/class_desk/class_desk.controller.ts
+++ src/Component/School_Setting/class_desk/class_desk.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
+import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
+import Role from 'src/Common/Guard/role.enum';
+import { Roles } from 'src/Common/Guard/roles.decorator';
 import { ClassDeskService } from './class_desk.service';
 import { CreateClassDeskDto } from './dto/create-class_desk.dto';
 import { UpdateClassDeskDto } from './dto/update-class_desk.dto';
+@UseGuards( JwtAuthGuard )
+@ApiTags( "Class-Desk" )
+@UseGuards( PrismaExceptionFilter )
 
-@Controller('class-desk')
-export class ClassDeskController {
-  constructor(private readonly classDeskService: ClassDeskService) {}
 
+@Controller( 'class-desk' )
+export class ClassDeskController
+{
+  constructor ( private readonly classDeskService: ClassDeskService ) { }
+
+  @Roles( Role.SuperAdmin )
   @Post()
-  create(@Body() createClassDeskDto: CreateClassDeskDto) {
-    return this.classDeskService.create(createClassDeskDto);
+  create ( @Body() createClassDeskDto: CreateClassDeskDto )
+  {
+    return this.classDeskService.create( createClassDeskDto );
   }
 
   @Get()
-  findAll() {
+  findAll ()
+  {
     return this.classDeskService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.classDeskService.findOne(+id);
+  @Get( 'school/:id' )
+  findAllBySchoolId ( @Param( 'id' ) id: string )
+  {
+    return this.classDeskService.findAllschoolClassId( +id );
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClassDeskDto: UpdateClassDeskDto) {
-    return this.classDeskService.update(+id, updateClassDeskDto);
+  @Get( ':id' )
+  findOne ( @Param( 'id' ) id: string )
+  {
+    return this.classDeskService.findOne( +id );
   }
+  @Roles( Role.SuperAdmin )
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.classDeskService.remove(+id);
+  @Patch( ':id' )
+  update ( @Param( 'id' ) id: string, @Body() updateClassDeskDto: UpdateClassDeskDto )
+  {
+    return this.classDeskService.update( +id, updateClassDeskDto );
+  }
+  @Roles( Role.SuperAdmin )
+
+  @Delete( ':id' )
+  remove ( @Param( 'id' ) id: string )
+  {
+    return this.classDeskService.remove( +id );
   }
 }
