929632576b879ee4257f73ba271207e7a15e7fc1
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 12:03:37 +0300

    🚀 feat: Add Student Barcodes functionality
    - Added Student Barcodes functionality to the project.
    - Implemented endpoints for creating, updating, and deleting student barcodes.
    - Added validation for the CreateStudentBarcodeDto class fields.
    - Updated the StudentBarcodesService to work with Prisma for database operations.

--- src/Component/Mission/student_barcodes/dto/create-student_barcode.dto.ts
+++ src/Component/Mission/student_barcodes/dto/create-student_barcode.dto.ts
@@ -1,1 +1,17 @@
-export class CreateStudentBarcodeDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateStudentBarcodeDto
+{
+    @ApiProperty()
+    @IsNumber()
+    Exam_Mission_ID: number;
+
+    @ApiProperty()
+    @IsNumber()
+    Student_ID: number;
+
+    @ApiProperty()
+    @IsString()
+    Barcode: string;
+}
