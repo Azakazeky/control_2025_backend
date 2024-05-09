c471c6c8f5298712c38c726170518a60562cadf2
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-09 09:27:51 +0300

    ✨ Update cohort controller and service for role-based access control
    - Refactor cohort controller and service to include role-based access control using
      the SuperAdmin role for create, update, and delete operations. Ensure only
      users with the SuperAdmin role can perform these actions. Add Roles decorator
      to controller methods for SuperAdmin role authentication. Update create-cohort
      DTO with ApiProperty and class-validator decorators for School_Type_ID and Name
      fields. Add PrismaService dependency injection to cohort service for
      interacting with the database. Update service methods to use PrismaService for
      creating, finding, updating, and deleting cohort data.

--- src/Component/School_Setting/cohort/cohort.service.ts
+++ src/Component/School_Setting/cohort/cohort.service.ts
@@ -1,26 +1,71 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/Common/Db/prisma.service';
 import { CreateCohortDto } from './dto/create-cohort.dto';
 import { UpdateCohortDto } from './dto/update-cohort.dto';
 
 @Injectable()
-export class CohortService {
-  create(createCohortDto: CreateCohortDto) {
-    return 'This action adds a new cohort';
+export class CohortService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
+
+  async create ( createCohortDto: CreateCohortDto )
+  {
+    var result = await this.prismaService.cohort.create( {
+      data: createCohortDto
+    } );
+    return result;
   }
 
-  findAll() {
-    return `This action returns all cohort`;
+  async findAll ()
+  {
+    var schools = await this.prismaService.cohort.findMany( {
+
+    } );
+
+    return schools;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} cohort`;
+  // TODO: need to check how to filter
+  // async findAllBySchoolId ( schoolId: number )
+  // {
+  //   var schools = await this.prismaService.cohort.findMany( {
+  //     where: {
+
+  //     }
+  //   } );
+  //   return schools;
+  // }
+
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.cohort.findUnique( {
+      where: {
+        ID: id
+      },
+
+    } );
+    return result;
   }
 
-  update(id: number, updateCohortDto: UpdateCohortDto) {
-    return `This action updates a #${id} cohort`;
+  async update ( id: number, updateCohortDto: UpdateCohortDto )
+  {
+    var result = await this.prismaService.cohort.update( {
+      where: {
+        ID: id
+      },
+      data: updateCohortDto
+    } );
+    return result;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} cohort`;
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.cohort.delete( {
+      where: {
+        ID: id
+      }
+    } );
+    return result;
   }
+
 }
