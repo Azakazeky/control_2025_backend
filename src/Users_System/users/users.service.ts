09108f214cb3de168e5cce72e56b6b30498a9e5f
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-05-13 08:45:53 +0300

    ✨ Add API endpoints to add roles and schools to a user
    New API endpoints added to allow the addition of roles and schools to a user
    using PATCH methods. The changes include adding new DTO classes and updating
    the user service to handle the addition of roles and schools to a user.

--- src/Users_System/users/users.service.ts
+++ src/Users_System/users/users.service.ts
@@ -1,92 +1,132 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/Common/Db/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, CreateUserHasRolesDto, CreateUserHasSchoolsDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 
 @Injectable()
-export class UsersService {
-  constructor(private readonly prismaService: PrismaService) { }
+export class UsersService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
 
-  async create(createUserCreateUserDto: CreateUserDto) {
-    var result = await this.prismaService.users.create({
+  async create ( createUserCreateUserDto: CreateUserDto )
+  {
+    var result = await this.prismaService.users.create( {
       data: createUserCreateUserDto
-    });
+    } );
     return result;
   }
 
-  async findAll() {
-    var results = await this.prismaService.users.findMany({
+  async findAll ()
+  {
+    var results = await this.prismaService.users.findMany( {
 
-    });
+    } );
 
     return results;
   }
 
+  async AddRolesToUser ( id: number, userHasRoles: CreateUserHasRolesDto[] )
+  {
+    var result = await this.prismaService.users.update( {
+      where: {
+        ID: id
+      },
+      data: {
+        users_has_roles: {
+          createMany: {
+            data: userHasRoles
+          }
+        }
+      }
+    } );
+    return result;
+  } async AddSchoolsToUser ( id: number, userHasRoles: CreateUserHasSchoolsDto[] )
+  {
+    var result = await this.prismaService.users.update( {
+      where: {
+        ID: id
+      },
+      data: {
+        users_has_schools: {
+          createMany: {
+            data: userHasRoles
+          }
+        }
+      }
+    } );
+    return result;
+  }
 
-  async findOne(id: number) {
-    var result = await this.prismaService.users.findUnique({
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.users.findUnique( {
       where: {
         ID: id
       },
 
-    });
+    } );
     return result;
   }
 
 
-  async findOneByUserName(userName: string) {
-    var result = await this.prismaService.users.findUnique({
+  async findOneByUserName ( userName: string )
+  {
+    var result = await this.prismaService.users.findUnique( {
       where: {
         User_Name: userName
       },
 
-    });
+    } );
     return result;
   }
 
-  async update(id: number, updateUserCreateUserDto: UpdateUserDto) {
-    var result = await this.prismaService.users.update({
+  async update ( id: number, updateUserCreateUserDto: UpdateUserDto )
+  {
+    var result = await this.prismaService.users.update( {
       where: {
         ID: id
       },
       data: updateUserCreateUserDto
-    });
+    } );
     return result;
   }
 
-  async remove(id: number) {
-    var result = await this.prismaService.users.delete({
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.users.delete( {
       where: {
         ID: id
       }
-    });
+    } );
     return result;
   }
 
   ///////******************* Proctors */
 
 
-  async findOneProctor(id: number) {
-    var result = await this.prismaService.users.findUnique({
+  async findOneProctor ( id: number )
+  {
+    var result = await this.prismaService.users.findUnique( {
       where: {
         ID: id
       },
 
-    });
+    } );
     return result;
 
   }
-  async findOneProctorByUserName(userName: string) {
-     var result = await this.prismaService.users.findUnique({
-       where: {
-         User_Name: userName
-       },
- 
-     });
-     return result;
- 
-   }
+  async findOneProctorByUserName ( userName: string )
+  {
+    var result = await this.prismaService.users.findUnique( {
+      where: {
+        User_Name: userName
+      },
+
+    } );
+    return result;
+
+  }
 
 
 }
