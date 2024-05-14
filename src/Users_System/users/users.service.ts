073f049be24831625bf4bbf1b275d65364d01e72
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-05-14 09:34:09 +0300

    edit users

--- src/Users_System/users/users.service.ts
+++ src/Users_System/users/users.service.ts
@@ -5,29 +5,25 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 
 @Injectable()
-export class UsersService
-{
-  constructor ( private readonly prismaService: PrismaService ) { }
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) { }
 
-  async create ( createUserCreateUserDto: CreateUserDto )
-  {
-    var result = await this.prismaService.users.create( {
+  async create(createUserCreateUserDto: CreateUserDto) {
+    var result = await this.prismaService.users.create({
       data: createUserCreateUserDto
-    } );
+    });
     return result;
   }
 
-  async findAll ()
-  {
-    var results = await this.prismaService.users.findMany( {
+  async findAll() {
+    var results = await this.prismaService.users.findMany({
 
-    } );
+    });
 
     return results;
   }
 
-  async AddRolesToUser ( id: number, userHasRoles: CreateUserHasRolesDto[] )
-  {
+  async AddRolesToUser(id: number, userHasRoles: CreateUserHasRolesDto[]) {
 
     var result = await this.prismaService.users.update({
       where: {
@@ -40,11 +36,10 @@ export class UsersService
           }
         }
       }
-    } );
+    });
     return result;
-  } async AddSchoolsToUser ( id: number, userHasRoles: CreateUserHasSchoolsDto[] )
-  {
-    var result = await this.prismaService.users.update( {
+  } async AddSchoolsToUser(id: number, userHasRoles: CreateUserHasSchoolsDto[]) {
+    var result = await this.prismaService.users.update({
       where: {
         ID: id
       },
@@ -55,76 +50,70 @@ export class UsersService
           }
         }
       }
-    } );
+    });
     return result;
   }
 
-  async findOne ( id: number )
-  {
-    var result = await this.prismaService.users.findUnique( {
+  async findOne(id: number) {
+    var result = await this.prismaService.users.findUnique({
       where: {
         ID: id
       },
 
-    } );
+    });
     return result;
   }
 
 
-  async findOneByUserName ( userName: string )
-  {
-    var result = await this.prismaService.users.findUnique( {
+  async findOneByUserName(userName: string) {
+    var result = await this.prismaService.users.findUnique({
       where: {
         User_Name: userName
       },
 
-    } );
+    });
     return result;
   }
 
-  async update ( id: number, updateUserCreateUserDto: UpdateUserDto )
-  {
-    var result = await this.prismaService.users.update( {
+  async update(id: number, updateUserCreateUserDto: UpdateUserDto) {
+    var result = await this.prismaService.users.update({
       where: {
         ID: id
       },
       data: updateUserCreateUserDto
-    } );
+    });
     return result;
   }
 
-  async remove ( id: number )
-  {
-    var result = await this.prismaService.users.delete( {
+  async remove(id: number) {
+    var result = await this.prismaService.users.delete({
       where: {
         ID: id
       }
-    } );
+    });
     return result;
   }
 
   ///////******************* Proctors */
 
 
-  async findOneProctor ( id: number )
-  {
-    var result = await this.prismaService.users.findUnique( {
+  async findOneProctor(id: number) {
+    var result = await this.prismaService.users.findUnique({
       where: {
         ID: id
       },
 
-    } );
+    });
     return result;
 
   }
-  async findOneProctorByUserName ( userName: string )
-  {
-    var result = await this.prismaService.users.findUnique( {
+  async findOneProctorByUserName(userName: string) {
+    var result = await this.prismaService.users.findUnique({
       where: {
         User_Name: userName
       },
 
-    } );
+    });
     return result;
 
   }
