13ddb938e1e7cf71eb1cd3babd0d0e7a5f3e4a52
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-16 09:23:05 +0300

    ✨ feat(proctor): remove PrismaExceptionFilter from controller
    Removes the `PrismaExceptionFilter` from the `ProctorController` as it
    is not necessary for this module. This simplifies the controller and
    reduces unnecessary code.

--- src/Component/proctor/proctor.controller.ts
+++ src/Component/proctor/proctor.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseFilters, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
 import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
 import Role from 'src/Common/Guard/role.enum';
 import { Roles } from 'src/Common/Guard/roles.decorator';
@@ -9,7 +8,6 @@ import { UpdateProctorDto } from './dto/update-proctor.dto';
 import { ProctorService } from './proctor.service';
 @UseGuards( JwtAuthGuard )
 @ApiTags( "proctor" )
-@UseFilters( PrismaExceptionFilter )
 @Controller( 'proctor' )
 export class ProctorController
 {
