196b0d736eb54ed1cefbf54e979ba8fcb9ade026
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-16 09:19:12 +0300

    ✨🔥 refactor(prisma-filter): update error responses for better UX
    The changes made in this commit focus on improving the error responses
    returned by the PrismaExceptionFilter. The main updates include:
    
    - Replacing `response.json()` with `response.send()` to simplify the
      response format and remove unnecessary properties like `statusCode`
      and `timestamp`.
    - Adjusting the error messages to provide more user-friendly information
      about the specific errors encountered, such as which field caused the
      constraint violation or what type of value was out of range.
    - Removing the `timestamp` property from the error responses, as this
      information is not essential for the client to handle the errors.
    
    These changes aim to make the error responses more concise and
    informative, enhancing the overall user experience when dealing with
    database-related errors in the application.

--- src/Common/Db/prisma.filter.ts
+++ src/Common/Db/prisma.filter.ts
@@ -15,52 +15,40 @@ export class PrismaExceptionFilter implements ExceptionFilter
     const request = ctx.getRequest<Request>();
     const status = exception.code;
 
-    console.log( exception.message );
-
-
     switch ( status )
     {
-
-      case "P2002":
-        return response
-          .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
-            message: "Unique constraint failed on the constraint: " + exception.meta.target,
-          } );
 
       case "P1003":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Database {database_file_name} does not exist at {database_file_path}"
           } );
       case "P1008":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "TimeOut from database server"
           } );
 
       case "P1010":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "User was denied access on the database"
           } );
       case "P1012":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Argument is missing"
           } );
 
@@ -68,18 +56,18 @@ export class PrismaExceptionFilter implements ExceptionFilter
       case "P1016":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Your raw query had an incorrect number of parameters"
           } );
 
       case "P1017":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Server has closed the connection."
           } );
 
@@ -87,9 +75,9 @@ export class PrismaExceptionFilter implements ExceptionFilter
       case "P2000":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "The provided value for the column is too long for the column's type."
           } );
 
@@ -97,18 +85,18 @@ export class PrismaExceptionFilter implements ExceptionFilter
       case "P2001":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "The record searched for in the where condition does not exist"
           } );
 
       case "P2002":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "This value violates an existing unique constraint on the field: " + exception.meta.field_name
           } );
 
@@ -116,28 +104,28 @@ export class PrismaExceptionFilter implements ExceptionFilter
       case "P2003":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
-            message: "Foreign key constraint failed on the field " + exception.meta.field_name,
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
+            message: "This value already exists for the field " + exception.meta.field_name,
           } );
 
 
       case "P2004":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Foreign key constraint failed on the field " + exception.meta.field_name,
           } );
 
       case "P2005":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "The value  stored in the database for the field  is invalid for the field's type" + exception.meta.field_name,
           } );
 
@@ -146,42 +134,42 @@ export class PrismaExceptionFilter implements ExceptionFilter
       case "P2006":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "The provided value is not valid" + exception.meta.field_name,
           } );
 
       case "P2007":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Data validation errors" + exception.meta.field_name,
           } );
       case "P2011":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Null constraint violation on the " + exception.meta.constraint,
           } );
       case "P2013":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Missing the required argument " + exception.meta.argument_name + " for field " + exception.meta.field_name + ' in tabale ' + exception.meta.object_name,
           } );
       case "P2015":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "A related record could not be found" + exception.meta.details,
           } );
 
@@ -189,35 +177,35 @@ export class PrismaExceptionFilter implements ExceptionFilter
       case "P2020":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Value out of range for the type " + exception.meta.details,
           } );
 
       case "P2027":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Multiple errors occurred on the database during query execution " + exception.meta.errors,
           } );
 
       case "P2033":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "A number used in the query does not fit into a 64 bit signed integer. Consider using BigInt as field type if you're trying to store large integers",
           } );
       case "P2025":
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: "Record to update not found",
           } );
 
@@ -225,9 +213,9 @@ export class PrismaExceptionFilter implements ExceptionFilter
 
         return response
           .status( 500 )
-          .json( {
-            statusCode: status,
-            timestamp: new Date().toISOString(),
+          .send( {
+            status: false,
+            // timestamp: new Date().toISOString(),
             message: exception.message + " =>meta:::" + exception.meta,
           } );
     }
