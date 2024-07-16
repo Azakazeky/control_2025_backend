4272ba2a7c93a97c44aca72d4cce23d18af599b3
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-16 09:22:05 +0300

    🔥 feat(School_Settings): Optimize imports and improve code structure
    This commit optimizes the imports and improves the code structure in the `school_classes.controller.ts` file. The key changes are:
    
    - Removed the `PrismaExceptionFilter` import, as it is no longer needed.
    - Simplified the imports by removing the unused `Delete` and `Param` imports.
    - Removed the `@UseFilters(PrismaExceptionFilter)` decorator, as it is no longer required.
    
    These changes help to streamline the code and make it more maintainable.

--- src/Component/School_Setting/school_classes/school_classes.controller.ts
+++ src/Component/School_Setting/school_classes/school_classes.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseFilters, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
 import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
 import Role from 'src/Common/Guard/role.enum';
 import { Roles } from 'src/Common/Guard/roles.decorator';
@@ -9,7 +8,6 @@ import { UpdateSchoolClassDto } from './dto/update-school_class.dto';
 import { SchoolClassesService } from './school_classes.service';
 @UseGuards( JwtAuthGuard )
 @ApiTags( "School-Classes" )
-@UseFilters( PrismaExceptionFilter )
 @Controller( 'school-classes' )
 export class SchoolClassesController
 {
