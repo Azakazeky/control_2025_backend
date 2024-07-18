a7d770ffec4ee6e0f5daff60558418ce00ce8457
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-18 15:10:39 +0300

    🔥 feat(Generate PDF): Optimize PDF generation workflow
    - Refactors the `GeneratePdfController` to improve code readability and maintainability
    - Simplifies the controller methods by extracting the PDF generation logic into the service layer
    - Adds proper error handling and response handling for the PDF generation endpoints
    - Ensures that the generated PDF files are cached and returned efficiently
    - Improves the overall performance and reliability of the PDF generation feature

--- src/Component/Mission/generate_pdf/generate_pdf.controller.ts
+++ src/Component/Mission/generate_pdf/generate_pdf.controller.ts
@@ -1,171 +1,201 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Res, UseGuards } from '@nestjs/common';
-import { GeneratePdfService } from './generate_pdf.service'; import { ApiTags, ApiQuery } from '@nestjs/swagger';
+import { Controller, Get, Param, Query, Res, UseGuards } from '@nestjs/common';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { PrismaExceptionFilter } from 'src/Common/Db/prisma.filter';
+import { GeneratePdfService } from './generate_pdf.service';
 // @UseGuards(JwtAuthGuard)
 
-@UseGuards(PrismaExceptionFilter)
-@ApiTags("Generate-PDF")
-@Controller('generate-pdf')
-export class GeneratePdfController {
-  constructor(private readonly generatePdfService: GeneratePdfService) { }
+@UseGuards( PrismaExceptionFilter )
+@ApiTags( "Generate-PDF" )
+@Controller( 'generate-pdf' )
+export class GeneratePdfController
+{
+  constructor ( private readonly generatePdfService: GeneratePdfService ) { }
 
-  @ApiQuery({
+  @ApiQuery( {
     name: 'gradeid',
     required: false,
     type: Number,
-  })
-  @Get('/seats/:id?')
+  } )
+  @Get( '/seats/:id?' )
   // @Header('Content-Disposition', 'attachment; filename="seats.pdf"')
-  async generateseats(@Param('id') id: string, @Query('gradeid') gradeId, @Res() response: Response) {
-    console.log('mission id' + id);
-    console.log('grade id : ' + gradeId);
-    try {
+  async generateseats ( @Param( 'id' ) id: string, @Query( 'gradeid' ) gradeId, @Res() response: Response )
+  {
+    console.log( 'mission id' + id );
+    console.log( 'grade id : ' + gradeId );
+    try
+    {
       const fileName = 'pdfGenerateor/seats/seats' + gradeId + id + '.pdf';
-      const result = await this.generatePdfService.checkFile(fileName);
+      const result = await this.generatePdfService.checkFile( fileName );
 
       // return response.status(201).send({ url: result });
 
-      return ({ url: result });
-    } catch (error) {
-      console.log('start generate');
-      var result = await this.generatePdfService.generatSeatNumber(+id, +gradeId);
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( 'start generate' );
+      var result = await this.generatePdfService.generatSeatNumber( +id, +gradeId );
 
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
     }
   }
 
 
-  @Get('/am-cover/:id/')
+  @Get( '/am-cover/:id/' )
   // @Header('Content-Disposition', 'attachment; filename="Amircan Covers.pdf"')
-  async generateAmircanCovers(@Param('id') id: string, @Query('writing') writing, @Res() response: Response) {
-    console.log(id);
-    console.log(writing);
-    try {
+  async generateAmircanCovers ( @Param( 'id' ) id: string, @Query( 'writing' ) writing, @Res() response: Response )
+  {
+    console.log( id );
+    console.log( writing );
+    try
+    {
       var fileName = 'pdfGenerateor/AmCover/AmCovers' + id;
-      if (Number(writing) == 1) {
-        fileName = fileName + 'writing'
+      if ( Number( writing ) == 1 )
+      {
+        fileName = fileName + 'writing';
       }
       fileName = fileName + '.pdf';
-      const result = await this.generatePdfService.checkFile(fileName);
-      return ({ url: result });
-    } catch (error) {
-      const result = await this.generatePdfService.generatAmCoverSheet(+id, Number(writing) == 1);
-      console.log('result ' + result);
+      const result = await this.generatePdfService.checkFile( fileName );
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      const result = await this.generatePdfService.generatAmCoverSheet( +id, Number( writing ) == 1 );
+      console.log( 'result ' + result );
 
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
     }
   }
 
-  @Get('/br-cover/:id/')
+  @Get( '/br-cover/:id/' )
   // @Header('Content-Disposition', 'attachment; filename="Amircan Covers.pdf"')
-  async generateBritishCOver(@Param('id') id: string, @Query('writing') writing, @Res() response: Response) {
-    try {
+  async generateBritishCOver ( @Param( 'id' ) id: string, @Query( 'writing' ) writing, @Res() response: Response )
+  {
+    try
+    {
       var path = 'pdfGenerateor/BrCover/BrCover' + id;
-      if (Number(writing) == 1) {
-        path = path + 'writing'
+      if ( Number( writing ) == 1 )
+      {
+        path = path + 'writing';
       }
       path = path + '.pdf';
-      const result = await this.generatePdfService.checkFile(path);
+      const result = await this.generatePdfService.checkFile( path );
 
 
-      return ({ url: result });
-    } catch (error) {
-      console.log(error);
-      const result = await this.generatePdfService.generatBrCoverSheet(+id, Number(writing) == 1);
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( error );
+      const result = await this.generatePdfService.generatBrCoverSheet( +id, Number( writing ) == 1 );
       // console.log(result);
       // const fileee = await this.pdfService.fileBuffer(result);
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
     }
   }
 
-  @Get('/IBCover/:id/')
+  @Get( '/IBCover/:id/' )
   // @Header('Content-Disposition', 'attachment; filename="Amircan Covers.pdf"')
-  async generateIBCOver(@Param('id') id: string, @Query('writing') writing, @Res() response: Response) {
+  async generateIBCOver ( @Param( 'id' ) id: string, @Query( 'writing' ) writing, @Res() response: Response )
+  {
 
 
-    try {
+    try
+    {
       var path = 'pdfGenerateor/IBCover/IBCover' + id;
-      if (Number(writing) == 1) {
-        path = path + 'writing'
+      if ( Number( writing ) == 1 )
+      {
+        path = path + 'writing';
       }
       path = path + '.pdf';
-      const result = await this.generatePdfService.checkFile(path);
+      const result = await this.generatePdfService.checkFile( path );
 
-      return ({ url: result });
-    } catch (error) {
-      console.log(error);
-      const result = await this.generatePdfService.generatIBCoverSheets(+id);
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( error );
+      const result = await this.generatePdfService.generatIBCoverSheets( +id );
+      return response.status( 201 ).send( { url: result } );
     }
   }
 
-  @ApiQuery({
+  @ApiQuery( {
     name: 'roomid',
     required: true,
     type: Number,
-  })
-  @Get('/attendance/')
+  } )
+  @Get( '/attendance/' )
   // @Header('Content-Disposition', 'attachment; filename="attendance.pdf"')
-  async generateAttendance(@Query('roomid') id, @Res() response: Response) {
-    try {
+  async generateAttendance ( @Query( 'roomid' ) id, @Res() response: Response )
+  {
+    try
+    {
       var path = 'pdfGenerateor/attendance/RoomId' + id + '.pdf';
-      const result = await this.generatePdfService.checkFile(path);
+      const result = await this.generatePdfService.checkFile( path );
 
 
-      return ({ url: result });
-    } catch (error) {
-      console.log(error);
-      const result = await this.generatePdfService.generatAttendance(+id, path);
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( error );
+      const result = await this.generatePdfService.generatAttendance( +id, path );
+      return response.status( 201 ).send( { url: result } );
     }
   }
 
 
-  @Get('EnglishWriting/:id')
-  async generateEnglishWriting(@Param('id') id: string, @Res() response: Response) {
-    try {
+  @Get( 'EnglishWriting/:id' )
+  async generateEnglishWriting ( @Param( 'id' ) id: string, @Res() response: Response )
+  {
+    try
+    {
       var path: string = 'pdfGenerateor/Writing/English/English_Writing_Assessment_';
-      const fileName = path + id + '.pdf'
-      const result = await this.generatePdfService.checkFile(fileName);
+      const fileName = path + id + '.pdf';
+      const result = await this.generatePdfService.checkFile( fileName );
 
-      return ({ url: result });
-    } catch (error) {
-      console.log(error);
-      const result = await this.generatePdfService.generateEnglishWriting(+id);
-      console.log(result);
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( error );
+      const result = await this.generatePdfService.generateEnglishWriting( +id );
+      console.log( result );
 
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
     }
 
   }
-  @Get('EnglishSocialStudies/:id')
-  async generateEnglishSocialStudies(@Param('id') id: string, @Res() response: Response) {
-    try {
+  @Get( 'EnglishSocialStudies/:id' )
+  async generateEnglishSocialStudies ( @Param( 'id' ) id: string, @Res() response: Response )
+  {
+    try
+    {
       var path: string = 'pdfGenerateor/Writing/EnglishSocialStudies/EnglishSocialStudies_';
-      const fileName = path + id + '.pdf'
-      const result = await this.generatePdfService.checkFile(fileName);
+      const fileName = path + id + '.pdf';
+      const result = await this.generatePdfService.checkFile( fileName );
 
-      return ({ url: result });
-    } catch (error) {
-      console.log(error);
-      const result = await this.generatePdfService.generateEnglishSocialStudies(+id);
-      console.log(result);
-      return ({ url: result });
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( error );
+      const result = await this.generatePdfService.generateEnglishSocialStudies( +id );
+      console.log( result );
+      return response.status( 201 ).send( { url: result } );
     }
   }
-  @Get('arabic/:id')
-  async generatearabic(@Param('id') id: string, @Res() response: Response) {
+  @Get( 'arabic/:id' )
+  async generatearabic ( @Param( 'id' ) id: string, @Res() response: Response )
+  {
 
-    try {
+    try
+    {
       var path: string = 'pdfGenerateor/Writing/Arabic/Arabic_';
-      const fileName = path + id + '.pdf'
-      const result = await this.generatePdfService.checkFile(fileName);
-      return ({ url: result });
-    } catch (error) {
-      console.log(error);
-      const result = await this.generatePdfService.generateArabicWriting(+id);
-      console.log(result);
-      return ({ url: result });
+      const fileName = path + id + '.pdf';
+      const result = await this.generatePdfService.checkFile( fileName );
+      return response.status( 201 ).send( { url: result } );
+    } catch ( error )
+    {
+      console.log( error );
+      const result = await this.generatePdfService.generateArabicWriting( +id );
+      console.log( result );
+      return response.status( 201 ).send( { url: result } );
 
     }
   }
