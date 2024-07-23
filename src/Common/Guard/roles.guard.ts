ea7c24331ca48346d9a6a6c471015863904c6c2d
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-23 13:00:10 +0300

    🚀 feat(roles.guard): improve user role validation
    Refactor the `RolesGuard` to fetch user roles from the request headers instead of the `request.user` object. This change ensures that the guard can be used in scenarios where the user object is not available in the request, making the implementation more flexible and reusable.

--- src/Common/Guard/roles.guard.ts
+++ src/Common/Guard/roles.guard.ts
@@ -1,22 +1,20 @@
-
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) { }
+  constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
     if (!roles) {
       return true;
     }
     const request = context.switchToHttp().getRequest();
-    const user = request.user;
-  
-   var matchRoles=  this.matchRoles(roles, user.roles);
-   return matchRoles;
+    const user = request.headers.user;
+
+    var matchRoles = this.matchRoles(roles, user.roles);
+    return matchRoles;
   }
 
   matchRoles(roles: string[], userRoles: string[]): boolean {
