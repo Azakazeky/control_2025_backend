3e43b563f05a01ef41904fa86b5b659b1dbc0453
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-23 09:55:51 +0300

    🏆 feat: Add School_Id field to CreateProctorDto
    The changes made in this commit include:
    
    - Added a new `School_Id` field to the `CreateProctorDto` class, which is of type `IsNumber()`.
    - This change is necessary to associate the proctor with a specific school, allowing for better organization and management of proctors in the system.

--- src/Component/proctor/dto/create-proctor.dto.ts
+++ src/Component/proctor/dto/create-proctor.dto.ts
@@ -1,61 +1,62 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsBoolean, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
 
-export class CreateProctorDto
-{
-    @ApiProperty()
-    @IsString()
-    Full_Name: string;
+export class CreateProctorDto {
+  @ApiProperty()
+  @IsNumber()
+  School_Id;
 
-    @ApiProperty()
-    @IsString()
-    User_Name: string;
+  @ApiProperty()
+  @IsString()
+  Full_Name: string;
 
-    @ApiProperty()
-    @IsString()
-    Password: string;
+  @ApiProperty()
+  @IsString()
+  User_Name: string;
 
-    @ApiProperty()
-    @IsString()
-    isFloorManager: string;
+  @ApiProperty()
+  @IsString()
+  Password: string;
 
-    @ApiProperty()
-    @IsString()
-    Division: string;
+  @ApiProperty()
+  @IsString()
+  isFloorManager: string;
 
-    @ApiProperty()
-    @IsNumber()
-    Created_By: number;
+  @ApiProperty()
+  @IsString()
+  Division: string;
+
+  @ApiProperty()
+  @IsNumber()
+  Created_By: number;
 }
 
-export class AssignProctorToExamRoomDto
-{
+export class AssignProctorToExamRoomDto {
+  @ApiProperty()
+  @IsNumber()
+  proctors_ID: number;
 
-    @ApiProperty()
-    @IsNumber()
-    proctors_ID: number;
+  @ApiProperty()
+  @IsNumber()
+  exam_room_ID: number;
 
-    @ApiProperty()
-    @IsNumber()
-    exam_room_ID: number;
+  @ApiProperty()
+  @IsString()
+  Month: string;
 
-    @ApiProperty()
-    @IsString()
-    Month: string;
+  @ApiProperty()
+  @IsString()
+  Year: string;
 
-    @ApiProperty()
-    @IsString()
-    Year: string;
+  @ApiProperty()
+  @IsBoolean()
+  Period?: boolean;
 
-    @ApiProperty()
-    @IsBoolean()
-    Period?: boolean;
+  @ApiProperty()
+  @IsString()
+  Attendance?: string;
 
-    @ApiProperty()
-    @IsString()
-    Attendance?: string;
-
-    @ApiProperty()
-    @IsNumber()
-    Created_By: number;
+  @ApiProperty()
+  @IsNumber()
+  Created_By: number;
 }
