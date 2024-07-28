d9e406bf3b1d950088f3428cc49fda3bd2b59f7b
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-28 15:32:42 +0300

    🎨 feat(school-classes): Add user ID to create school class
    This change adds the user's ID to the `create` method in the `SchoolClassesController`. This allows the `SchoolClassesService` to associate the created school class with the user who created it.
    
    The changes include:
    
    - Adding `@Req()` to the `create` method to get the user's ID from the request headers
    - Passing the user's ID to the `schoolClassesService.create()` method
    
    This feature will help to track the ownership and responsible user for each school class created in the system.

--- src/Component/School_Setting/school_classes/school_classes.controller.ts
+++ src/Component/School_Setting/school_classes/school_classes.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
 import Role from 'src/Common/Guard/role.enum';
@@ -6,64 +16,59 @@ import { Roles } from 'src/Common/Guard/roles.decorator';
 import { CreateSchoolClassDto } from './dto/create-school_class.dto';
 import { UpdateSchoolClassDto } from './dto/update-school_class.dto';
 import { SchoolClassesService } from './school_classes.service';
-@UseGuards( JwtAuthGuard )
-@ApiTags( "School-Classes" )
-@Controller( 'school-classes' )
-export class SchoolClassesController
-{
-  constructor ( private readonly schoolClassesService: SchoolClassesService ) { }
-  @Roles( Role.SuperAdmin )
-
+@UseGuards(JwtAuthGuard)
+@ApiTags('School-Classes')
+@Controller('school-classes')
+export class SchoolClassesController {
+  constructor(private readonly schoolClassesService: SchoolClassesService) {}
+  @Roles(Role.SuperAdmin)
   @Post()
-  create ( @Body() createSchoolClassDto: CreateSchoolClassDto )
-  {
-    return this.schoolClassesService.create( createSchoolClassDto );
+  create(
+    @Body() createSchoolClassDto: CreateSchoolClassDto,
+    @Req() req: Request,
+  ) {
+    return this.schoolClassesService.create(
+      createSchoolClassDto,
+      req.headers['user']['userId'],
+    );
   }
 
   @Get()
-  findAll ()
-  {
+  findAll() {
     return this.schoolClassesService.findAll();
   }
 
-  @Get( ':id' )
-  findOne ( @Param( 'id' ) id: string )
-  {
-    return this.schoolClassesService.findOne( +id );
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.schoolClassesService.findOne(+id);
   }
-  @Get( 'school/:id' )
-  findBySchoolId ( @Param( 'id' ) id: string )
-  {
-    return this.schoolClassesService.findBySchool( +id );
+  @Get('school/:id')
+  findBySchoolId(@Param('id') id: string) {
+    return this.schoolClassesService.findBySchool(+id);
   }
-  @Roles( Role.SuperAdmin )
-
-  @Patch( ':id' )
-  update ( @Param( 'id' ) id: string, @Body() updateSchoolClassDto: UpdateSchoolClassDto )
-  {
-    return this.schoolClassesService.update( +id, updateSchoolClassDto );
+  @Roles(Role.SuperAdmin)
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateSchoolClassDto: UpdateSchoolClassDto,
+  ) {
+    return this.schoolClassesService.update(+id, updateSchoolClassDto);
   }
-  @Roles( Role.SuperAdmin )
-
-  @Delete( ':id' )
-  remove ( @Param( 'id' ) id: string )
-  {
-    return this.schoolClassesService.remove( +id );
+  @Roles(Role.SuperAdmin)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.schoolClassesService.remove(+id);
   }
 
-  @Roles( Role.SuperAdmin )
-
-  @Patch( 'activate/:id' )
-  activate ( @Param( 'id' ) id: string )
-  {
-    return this.schoolClassesService.activate( +id );
+  @Roles(Role.SuperAdmin)
+  @Patch('activate/:id')
+  activate(@Param('id') id: string) {
+    return this.schoolClassesService.activate(+id);
   }
 
-  @Roles( Role.SuperAdmin )
-
-  @Patch( 'deactivate/:id' )
-  deactivate ( @Param( 'id' ) id: string )
-  {
-    return this.schoolClassesService.deactivate( +id );
+  @Roles(Role.SuperAdmin)
+  @Patch('deactivate/:id')
+  deactivate(@Param('id') id: string) {
+    return this.schoolClassesService.deactivate(+id);
   }
 }
