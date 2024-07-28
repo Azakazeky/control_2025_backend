f2162766d3156627bee35951be8354cca727f5db
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-28 15:32:35 +0300

    ✨feat: Add `Created_By` field to school class creation
    The changes made in this commit add the `Created_By` field to the `create` method of the `SchoolClassesService`. This allows the creation of a school class to be associated with the user who created it. This is an important feature to track the ownership and accountability of school class creation.

--- src/Component/School_Setting/school_classes/school_classes.service.ts
+++ src/Component/School_Setting/school_classes/school_classes.service.ts
@@ -3,93 +3,81 @@ import { PrismaService } from 'src/Common/Db/prisma.service';
 import { CreateSchoolClassDto } from './dto/create-school_class.dto';
 import { UpdateSchoolClassDto } from './dto/update-school_class.dto';
 
-
 @Injectable()
-export class SchoolClassesService
-{
-  constructor ( private readonly prismaService: PrismaService ) { }
+export class SchoolClassesService {
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async create ( createSchoolClassDto: CreateSchoolClassDto )
-  {
-    var result = await this.prismaService.school_class.create( {
-      data: createSchoolClassDto
-    } );
+  async create(createSchoolClassDto: CreateSchoolClassDto, createdBy: number) {
+    var result = await this.prismaService.school_class.create({
+      data: { ...createSchoolClassDto, Created_By: createdBy },
+    });
     return result;
   }
 
-  async findAll ()
-  {
-    var results = await this.prismaService.school_class.findMany( {
-
-    } );
+  async findAll() {
+    var results = await this.prismaService.school_class.findMany({});
 
     return results;
   }
 
-  async findOne ( id: number )
-  {
-    var result = await this.prismaService.school_class.findUnique( {
+  async findOne(id: number) {
+    var result = await this.prismaService.school_class.findUnique({
       where: {
-        ID: id
+        ID: id,
       },
-    } );
+    });
     return result;
   }
 
-  async findBySchool ( id: number )
-  {
-    var result = await this.prismaService.school_class.findMany( {
+  async findBySchool(id: number) {
+    var result = await this.prismaService.school_class.findMany({
       where: {
-        Schools_ID: id
+        Schools_ID: id,
       },
-    } );
+    });
     return result;
   }
 
-  async update ( id: number, updateSchoolClassDto: UpdateSchoolClassDto )
-  {
-    var result = await this.prismaService.school_class.update( {
+  async update(id: number, updateSchoolClassDto: UpdateSchoolClassDto) {
+    var result = await this.prismaService.school_class.update({
       where: {
-        ID: id
+        ID: id,
       },
-      data: updateSchoolClassDto
-    } );
+      data: updateSchoolClassDto,
+    });
     return result;
   }
 
-  async remove ( id: number )
-  {
-    var result = await this.prismaService.school_class.delete( {
+  async remove(id: number) {
+    var result = await this.prismaService.school_class.delete({
       where: {
-        ID: id
-      }
-    } );
+        ID: id,
+      },
+    });
     return result;
   }
 
-  async activate ( id: number )
-  {
-    var result = await this.prismaService.school_class.update( {
+  async activate(id: number) {
+    var result = await this.prismaService.school_class.update({
       where: {
-        ID: id
+        ID: id,
       },
       data: {
-        Active: 1
-      }
-    } );
+        Active: 1,
+      },
+    });
     return result;
   }
 
-  async deactivate ( id: number )
-  {
-    var result = await this.prismaService.school_class.update( {
+  async deactivate(id: number) {
+    var result = await this.prismaService.school_class.update({
       where: {
-        ID: id
+        ID: id,
       },
       data: {
-        Active: 0
-      }
-    } );
+        Active: 0,
+      },
+    });
     return result;
   }
 }
