3db492b78e5f774508bafa9054a18e416b0de724
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-29 08:09:43 +0300

    ✨ feat(mission): update CreateControlMissionDto properties
    Modifies the CreateControlMissionDto class to update the properties:
    - Removes the `Schools_ID` property and adds a `Name` property
    - Updates the data types of the properties to match the requirements

--- src/Component/Mission/control_mission/dto/create-control_mission.dto.ts
+++ src/Component/Mission/control_mission/dto/create-control_mission.dto.ts
@@ -1,25 +1,20 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
 
-export class CreateControlMissionDto
-{
-    @ApiProperty()
-    @IsNumber()
-    Education_year_ID: number;
+export class CreateControlMissionDto {
+  @ApiProperty()
+  @IsNumber()
+  Education_year_ID: number;
 
-    @ApiProperty()
-    @IsString()
-    Schools_ID: number;
+  @ApiProperty()
+  @IsString()
+  Name: string;
 
-    @ApiProperty()
-    @IsString()
-    Name: string;
+  @ApiProperty()
+  @IsDate()
+  Start_Date;
 
-    @ApiProperty()
-    @IsDate()
-    Start_Date;
-
-    @ApiProperty()
-    @IsDate()
-    End_Date;
+  @ApiProperty()
+  @IsDate()
+  End_Date;
 }
