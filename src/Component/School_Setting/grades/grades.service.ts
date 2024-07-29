d0c129504be0c8844e054c874e6a1751b7fdb5ef
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-29 08:10:18 +0300

    ✨ feat(grades): enhance grade service with school ID association
    The changes in this commit enhance the `GradesService` to associate grades with a specific school. The `create()` method now takes a `schoolId` parameter and adds it to the grade data before creating a new record. Additionally, the `findAllBySchoolId()` method has been added to retrieve all grades for a given school.
    
    These changes improve the functionality of the grade management system by allowing grades to be organized and retrieved based on the school they belong to.

--- src/Component/School_Setting/grades/grades.service.ts
+++ src/Component/School_Setting/grades/grades.service.ts
@@ -4,92 +4,79 @@ import { CreateGradeDto } from './dto/create-grade.dto';
 import { UpdateGradeDto } from './dto/update-grade.dto';
 
 @Injectable()
-export class GradesService
-{
-  constructor ( private readonly prismaService: PrismaService ) { }
+export class GradesService {
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async create ( createGradeDto: CreateGradeDto )
-  {
-    var result = await this.prismaService.grades.create( {
-      data: createGradeDto
-    } );
+  async create(createGradeDto: CreateGradeDto, schoolId: number) {
+    var result = await this.prismaService.grades.create({
+      data: { ...createGradeDto, Schools_ID: schoolId },
+    });
     return result;
   }
 
-  async findAll ()
-  {
-    var schools = await this.prismaService.grades.findMany( {
-
-    } );
+  async findAll() {
+    var schools = await this.prismaService.grades.findMany({});
 
     return schools;
   }
-  async findAllBySchoolId ( schoolId: number )
-  {
-    var schools = await this.prismaService.grades.findMany( {
+  async findAllBySchoolId(schoolId: number) {
+    var schools = await this.prismaService.grades.findMany({
       where: {
-        Schools_ID: schoolId
-      }
-    } );
+        Schools_ID: schoolId,
+      },
+    });
     return schools;
   }
 
-  async findOne ( id: number )
-  {
-    var result = await this.prismaService.grades.findUnique( {
+  async findOne(id: number) {
+    var result = await this.prismaService.grades.findUnique({
       where: {
-        ID: id
+        ID: id,
       },
-
-    } );
+    });
     return result;
   }
 
-  async update ( id: number, updateGradeDto: UpdateGradeDto )
-  {
-    var result = await this.prismaService.grades.update( {
+  async update(id: number, updateGradeDto: UpdateGradeDto) {
+    var result = await this.prismaService.grades.update({
       where: {
-        ID: id
+        ID: id,
       },
-      data: updateGradeDto
-    } );
+      data: updateGradeDto,
+    });
     return result;
   }
 
-  async remove ( id: number )
-  {
-    var result = await this.prismaService.grades.delete( {
+  async remove(id: number) {
+    var result = await this.prismaService.grades.delete({
       where: {
-        ID: id
-      }
-    } );
+        ID: id,
+      },
+    });
     return result;
   }
 
-  async activate ( id: number )
-  {
-    var result = await this.prismaService.grades.update( {
+  async activate(id: number) {
+    var result = await this.prismaService.grades.update({
       where: {
-        ID: id
+        ID: id,
       },
       data: {
-        Active: 1
-      }
-    } );
+        Active: 1,
+      },
+    });
     return result;
   }
 
-  async deactivate ( id: number )
-  {
-    var result = await this.prismaService.grades.update( {
+  async deactivate(id: number) {
+    var result = await this.prismaService.grades.update({
       where: {
-        ID: id
+        ID: id,
       },
       data: {
-        Active: 0
-      }
-    } );
+        Active: 0,
+      },
+    });
     return result;
   }
-
 }
