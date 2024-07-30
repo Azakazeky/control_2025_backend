43653ffacbbd776084740d9d164ddba9adae80f4
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-30 15:50:42 +0300

    ✨🔒 feat(WebsocketGateway): Enhance WebSocket authentication and room management
    - Implement JWT-based authentication for WebSocket connections
    - Optimize room management logic to handle multiple clients per room
    - Leverage room-based events to notify clients of new users joining the room
    - Refactor code structure for better readability and maintainability

--- src/websocket.gateway.ts
+++ src/websocket.gateway.ts
@@ -1,113 +1,128 @@
 import { OnModuleInit } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from "socket.io";
 import { AuthService } from './Component/auth/auth.service';
-import { JwtService } from '@nestjs/jwt';
 
 
-@WebSocketGateway(3000, {
-    transports: ['websocket']
-})
-export class WebsocketGateway implements OnModuleInit, OnGatewayDisconnect, OnGatewayConnection {
-    constructor(private authService: AuthService, private jwtService: JwtService) { }
+@WebSocketGateway( 3000, {
+    transports: [ 'websocket' ]
+} )
+export class WebsocketGateway implements OnModuleInit, OnGatewayDisconnect, OnGatewayConnection
+{
+    constructor ( private authService: AuthService, private jwtService: JwtService ) { }
     private rooms: Map<string, Array<Map<string, any>>[]> = new Map();
 
     @WebSocketServer()
     server: Server;
 
-    handleDisconnect(client: Socket) {
+    handleDisconnect ( client: Socket )
+    {
 
-        console.log('Disconnect ', client.id);
+        console.log( 'Disconnect ', client.id );
     }
-    async handleConnection(client: Socket, ...args: any[]) {
-        var roomId: any = client.handshake.headers.roomid;
-        if (await this.jwtService.decode(client.handshake.headers.authorization.split(' ')[1])) {
-            var tokenData = await this.jwtService.decode(client.handshake.headers.authorization.split(' ')[1]);
-            console.log(tokenData);
-            console.log(tokenData.userId);
-            client.join(roomId);
-            if (this.rooms.has(roomId)) {
-                this.rooms[roomId] = this.rooms[roomId].push({
+    async handleConnection ( client: Socket, ...args: any[] )
+    {
+
+
+        var roomId: any = client.handshake.query.roomid;
+        if ( client.handshake.auth.authorization && await this.jwtService.decode( client.handshake.auth.authorization.split( ' ' )[ 1 ] ) )
+        {
+            var tokenData = await this.jwtService.decode( client.handshake.auth.authorization.split( ' ' )[ 1 ] );
+            console.log( tokenData );
+            console.log( tokenData.userId );
+            client.join( roomId );
+            if ( this.rooms.has( roomId ) )
+            {
+                this.rooms[ roomId ] = this.rooms[ roomId ].push( {
                     'socket_id': client.id,
                     'user_id': tokenData.userId,
                     'type': tokenData.type
-                })
-            } else {
-                this.rooms[roomId] = [{
+                } );
+            } else
+            {
+                this.rooms[ roomId ] = [ {
                     'socket_id': client.id,
                     'user_id': tokenData.userId,
                     'type': tokenData.type
-                }];
+                } ];
             }
 
 
-            this.server.to(roomId).emit('newUser', this.rooms[roomId]);
+            this.server.to( roomId ).emit( 'newUser', this.rooms[ roomId ] );
 
         }
 
-        console.log('connected ', client.id);
+        console.log( 'connected ', client.id );
     }
 
-    onModuleInit() {
-        this.server.on('connection', (socket) => {
-            console.log('connected', socket.id, Date.now());
-        });
-    }
-
-
-    @SubscribeMessage('joinRoom')
-    async handleJoinRoom(client: Socket, message: { roomId: string }) {
-        // let auth_token = client.handshake.headers.authorization.split(' ')[1];
-        if (await this.jwtService.decode(client.handshake.headers.authorization.split(' ')[1])) {
-            var tokenData = await this.jwtService.decode(client.handshake.headers.authorization.split(' ')[1]);
-            console.log(tokenData);
-            console.log(tokenData.userId);
-            console.log('new user joined room', message.roomId);
-            client.join(message.roomId);
-            if (this.rooms.has(message.roomId)) {
-                this.rooms[message.roomId] = this.rooms[message.roomId].push({
-                    'socket_id': client.id,
-                    'user_id': tokenData.userId,
-                    'type': tokenData.type
-                })
-            } else {
-                this.rooms[message.roomId] = [{
-                    'socket_id': client.id,
-                    'user_id': tokenData.userId,
-                    'type': tokenData.type
-                }];
-            }
-
-
-            this.server.to(message.roomId).emit('newUser', this.rooms[message.roomId]);
-
-        }
-    }
-
-    @SubscribeMessage('leaveRoom')
-    handleLeaveRoom(client: any, data: { roomId: string; }): void {
-        console.log('user left room', data.roomId);
-        client.leave(data.roomId);
-        this.server.to(data.roomId).emit('userLeft', { userId: client.id });
-    }
-
-    @SubscribeMessage('deleteRoom')
-    handleDeleteRoom(client: any, data: { roomId: string; }): void {
-        this.server.socketsLeave(data.roomId);
-    }
-
-    @SubscribeMessage('onEvent')
-    handleMessage(client: any, data: { roomId: string; message: string; sender: string; }): void {
-        console.log('onEvent', data);
-        this.server.to(data.roomId).emit('onEvent', { message: data.message, sender: data.sender });
+    onModuleInit ()
+    {
+        this.server.on( 'connection', ( socket ) =>
+        {
+            console.log( 'connected', socket.id, Date.now() );
+        } );
     }
 
 
-    @SubscribeMessage('onStudent')
-    getStudent(client: Socket, message: { roomId: string }): void {
-        console.log(message);
+    // @SubscribeMessage( 'joinRoom' )
+    // async handleJoinRoom ( client: Socket, message: { roomId: string; } )
+    // {
+    // let auth_token = client.handshake.headers.authorization.split(' ')[1];
+    // if (await this.jwtService.decode(client.handshake.headers.authorization.split(' ')[1])) {
+    //     var tokenData = await this.jwtService.decode(client.handshake.headers.authorization.split(' ')[1]);
+    //     console.log(tokenData);
+    //     console.log(tokenData.userId);
+    //     console.log('new user joined room', message.roomId);
+    //     client.join(message.roomId);
+    //     if (this.rooms.has(message.roomId)) {
+    //         this.rooms[message.roomId] = this.rooms[message.roomId].push({
+    //             'socket_id': client.id,
+    //             'user_id': tokenData.userId,
+    //             'type': tokenData.type
+    //         })
+    //     } else {
+    //         this.rooms[message.roomId] = [{
+    //             'socket_id': client.id,
+    //             'user_id': tokenData.userId,
+    //             'type': tokenData.type
+    //         }];
+    //     }
 
-        this.server.to(message.roomId).emit('students', this.rooms[message.roomId]);
+
+    //     this.server.to(message.roomId).emit('newUser', this.rooms[message.roomId]);
+
+    // }
+    // }
+
+    @SubscribeMessage( 'leaveRoom' )
+    handleLeaveRoom ( client: any, data: { roomId: string; } ): void
+    {
+        console.log( 'user left room', data.roomId );
+        client.leave( data.roomId );
+        this.server.to( data.roomId ).emit( 'userLeft', { userId: client.id } );
+    }
+
+    @SubscribeMessage( 'deleteRoom' )
+    handleDeleteRoom ( client: any, data: { roomId: string; } ): void
+    {
+        this.server.socketsLeave( data.roomId );
+    }
+
+    @SubscribeMessage( 'onEvent' )
+    handleMessage ( client: any, data: { roomId: string; message: string; sender: string; } ): void
+    {
+        console.log( 'onEvent', data );
+        this.server.to( data.roomId ).emit( 'onEvent', { message: data.message, sender: data.sender } );
+    }
+
+
+    @SubscribeMessage( 'onStudent' )
+    getStudent ( client: Socket, message: { roomId: string; } ): void
+    {
+        console.log( message );
+
+        this.server.to( message.roomId ).emit( 'students', this.rooms[ message.roomId ] );
     }
 
 
