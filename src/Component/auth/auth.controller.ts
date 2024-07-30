f326bab71c09e42e369866f9faa0f4602c71c664
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-30 10:28:55 +0300

    ✨ feat(auth): add student login for exam functionality
    Adds a new endpoint `/auth/studentLoginForExam` to handle student login for exam
    functionality. This endpoint allows students to login and get access tokens for
    taking exams. The changes also include updated imports and formatting for
    consistency in the `auth.controller.ts` file.

--- src/Component/auth/auth.controller.ts
+++ src/Component/auth/auth.controller.ts
@@ -1,27 +1,38 @@
 import { Body, Controller, Delete, Ip, Post, Query, Req } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import RefreshTokenDto from './dto/refresh-token.dto';
-import { LoginDto } from './dto/login.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import RefreshTokenDto from './dto/refresh-token.dto';
 
 @ApiBearerAuth('JWT')
-@ApiTags("Users")
+@ApiTags('Users')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  login(@Query('mobile') mobile: number,   @Req() request, @Ip() ip: string, @Body() body: LoginDto) {
+  login(
+    @Query('mobile') mobile: number,
+    @Req() request,
+    @Ip() ip: string,
+    @Body() body: LoginDto,
+  ) {
     // geting the useragent and ip address from @Req decorator and @Ip decorater imported at the top.
     console.log(ip);
-    return this.authService.login(body.userName, body.password, +mobile
+    return this.authService.login(
+      body.userName,
+      body.password,
+      +mobile,
       // {
       // ipAddress: ip,
       // userAgent: request.headers['user-agent'],
       // }
-
     );
+  }
+
+  @Post('studentLoginForExam')
+  studentLoginForExam(@Body() body: LoginDto) {
+    return this.authService.studentLoginForExam(body.userName, body.password);
   }
 
   @Post('refresh')
@@ -31,9 +42,7 @@ export class AuthController {
 
   @Delete('logout')
   async logout(@Body() body: RefreshTokenDto) {
-    console.log('logout');   
+    console.log('logout');
     return this.authService.logout(body.refreshToken);
   }
-
 }
-
