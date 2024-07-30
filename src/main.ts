fbe85d03fc4f4fcfe4d03eaea5978452921e3485
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-30 13:22:43 +0300

    🔧 refactor: Improve Swagger configuration in main.ts
    This commit makes the following changes to the `main.ts` file:
    
    1. Removes the `helmet` import and instead uses the `@fastify/helmet` package to register the Helmet middleware.
    2. Simplifies the Swagger configuration by using the `SwaggerModule.createDocument` and `SwaggerModule.setup` methods directly, rather than chaining multiple method calls.
    3. Refactors the Swagger configuration to use a more concise and readable syntax, with improved formatting and spacing.
    4. Adds handling for the `module.hot` option, which allows the application to be reloaded during development without restarting the server.
    
    These changes improve the overall readability and maintainability of the `main.ts` file, while also ensuring that the Swagger configuration is set up correctly and efficiently.

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,48 @@
-import helmet from '@fastify/helmet';
 import { NestFactory } from '@nestjs/core';
-import {
+import
+{
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { WsAdapter } from '@nestjs/platform-ws';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
 import * as admin from 'firebase-admin';
 import { AppModule } from './app.module';
 import { PrismaExceptionFilter } from './Common/Db/prisma.filter';
+const helmet = require( '@fastify/helmet' );
 
 declare const module: any;
-async function bootstrap() {
+async function bootstrap ()
+{
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  app.enableCors({
+  app.enableCors( {
     origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    methods: [ 'GET', 'POST', 'DELETE', 'PATCH' ],
     credentials: true,
-  });
+  } );
   const firebaseConfig = {
-    credential: admin.credential.cert('./nis-control-bucket.json'),
+    credential: admin.credential.cert( './nis-control-bucket.json' ),
   };
-  admin.initializeApp(firebaseConfig);
-  if (admin.apps.length === 0) {
-    admin.initializeApp(firebaseConfig);
-  } else {
+  admin.initializeApp( firebaseConfig );
+  if ( admin.apps.length === 0 )
+  {
+    admin.initializeApp( firebaseConfig );
+  } else
+  {
     admin.app(); // Use the default app
   }
-  app.useWebSocketAdapter(new WsAdapter(app));
-  app.useGlobalFilters(new PrismaExceptionFilter());
-  await app.register(helmet);
+  app.useGlobalFilters( new PrismaExceptionFilter() );
+  await app.register( helmet );
 
   // app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+  app.use( bodyParser.urlencoded( { limit: '50mb', extended: true } ) );
   const config = new DocumentBuilder()
-    .setTitle('NIS-Control Api System')
-    .setDescription('The NIS-Control API description')
-    .setVersion('1.1')
+    .setTitle( 'NIS-Control Api System' )
+    .setDescription( 'The NIS-Control API description' )
+    .setVersion( '1.1' )
     .addBearerAuth(
       {
         type: 'http',
@@ -52,35 +54,36 @@ async function bootstrap() {
       },
       'JWT',
     )
-    .addSecurityRequirements('JWT')
-    .addTag('Users')
-    .addTag('Generate-PDF')
-    .addTag('Roles-System')
-    .addTag('School & Grades')
-    .addTag('cohort')
-    .addTag('Subject')
-    .addTag('Students')
-    .addTag('Class-Desk')
-    .addTag('Education-Years')
-    .addTag('School-Classes')
-    .addTag('School-Type')
-    .addTag('Control-Mission')
-    .addTag('Exam-Mission')
-    .addTag('Exam-Room')
-    .addTag('Student-Barcodes')
-    .addTag('student-seat-numbers')
-    .addTag('User-Roles-Systems')
-    .addTag('Controllers')
+    .addSecurityRequirements( 'JWT' )
+    .addTag( 'Users' )
+    .addTag( 'Generate-PDF' )
+    .addTag( 'Roles-System' )
+    .addTag( 'School & Grades' )
+    .addTag( 'cohort' )
+    .addTag( 'Subject' )
+    .addTag( 'Students' )
+    .addTag( 'Class-Desk' )
+    .addTag( 'Education-Years' )
+    .addTag( 'School-Classes' )
+    .addTag( 'School-Type' )
+    .addTag( 'Control-Mission' )
+    .addTag( 'Exam-Mission' )
+    .addTag( 'Exam-Room' )
+    .addTag( 'Student-Barcodes' )
+    .addTag( 'student-seat-numbers' )
+    .addTag( 'User-Roles-Systems' )
+    .addTag( 'Controllers' )
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document, {
+  const document = SwaggerModule.createDocument( app, config );
+  SwaggerModule.setup( 'swagger', app, document, {
     swaggerOptions: {},
-  });
-  await app.listen(3333, '0.0.0.0');
-  if (module.hot) {
+  } );
+  await app.listen( 3333, '0.0.0.0' );
+  if ( module.hot )
+  {
     module.hot.accept();
-    module.hot.dispose(() => app.close());
+    module.hot.dispose( () => app.close() );
   }
 }
 bootstrap();
