ce8ada08209f2947230207e47f8df0c4f7800eb2
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-31 14:49:26 +0300

    ✨feat: Enhance control mission functionality
    This commit introduces the following changes to the control mission functionality:
    
    - Refactors the controller code to improve readability and maintainability
    - Adds new endpoints for creating student seat numbers and fetching grades by control mission ID
    - Enhances the existing endpoints to support filtering by education year and school ID
    - Adds role-based access control to the endpoints using the `@Roles` decorator
    - Utilizes request headers to retrieve user and school-related information
    
    These changes aim to provide a more robust and flexible control mission management system, enabling better organization and access control for the application's users.

--- src/Component/Mission/control_mission/control_mission.controller.ts
+++ src/Component/Mission/control_mission/control_mission.controller.ts
@@ -1,4 +1,5 @@
-import {
+import
+{
   Body,
   Controller,
   Delete,
@@ -18,111 +19,124 @@ import { CreateControlMissionDto } from './dto/create-control_mission.dto';
 import { CreateStudentSeatNumberDto } from './dto/create-student-seat-numbers.dto';
 import { UpdateControlMissionDto } from './dto/update-control_mission.dto';
 
-@UseGuards(JwtAuthGuard)
-@ApiTags('Control-Mission')
-@Controller('control-mission')
-export class ControlMissionController {
-  constructor(private readonly controlMissionService: ControlMissionService) {}
+@UseGuards( JwtAuthGuard )
+@ApiTags( 'Control-Mission' )
+@Controller( 'control-mission' )
+export class ControlMissionController
+{
+  constructor ( private readonly controlMissionService: ControlMissionService ) { }
 
-  @Roles(Role.SuperAdmin)
+  @Roles( Role.SuperAdmin )
   @Post()
-  create(
+  create (
     @Body() createControlMissionDto: CreateControlMissionDto,
     @Req() req: Request,
-  ) {
+  )
+  {
     return this.controlMissionService.create(
       createControlMissionDto,
-      req.headers['user']['userId'],
-      req.headers['user']['Schools_ID'],
+      req.headers[ 'user' ][ 'userId' ],
+      req.headers[ 'user' ][ 'Schools_ID' ],
     );
   }
 
-  @Roles(Role.SuperAdmin)
-  @ApiBody({ type: CreateStudentSeatNumberDto })
-  @Post('student-seat-numbers')
-  createStudentSeatNumbers(
+  @Roles( Role.SuperAdmin )
+  @ApiBody( { type: CreateStudentSeatNumberDto } )
+  @Post( 'student-seat-numbers' )
+  createStudentSeatNumbers (
     @Body() createStudentSeatNumberDto: CreateStudentSeatNumberDto,
-  ) {
+  )
+  {
     return this.controlMissionService.createStudentSeatNumbers(
       createStudentSeatNumberDto,
     );
   }
 
   @Get()
-  findAll() {
+  findAll ()
+  {
     return this.controlMissionService.findAll();
   }
 
-  @Get('school/:schoolId/education-year/:educationYearId')
-  findAllByEducationYearIdAndSchoolId(
-    @Param('educationYearId') educationYearId: string,
+  @Get( 'school/:schoolId/education-year/:educationYearId' )
+  findAllByEducationYearIdAndSchoolId (
+    @Param( 'educationYearId' ) educationYearId: string,
     @Req() req: Request,
-  ) {
+  )
+  {
     return this.controlMissionService.findAllByEducationYearIdAndSchoolId(
-      +req.headers['user']['Schools_ID'],
+      +req.headers[ 'user' ][ 'schoolId' ],
       +educationYearId,
     );
   }
 
-  @Get('schoolId/')
-  findAllBySchoolId(@Req() req: Request) {
+  @Get( 'schoolId/' )
+  findAllBySchoolId ( @Req() req: Request )
+  {
     return this.controlMissionService.findAllBySchoolId(
-      +req.headers['user']['Schools_ID'],
+      +req.headers[ 'user' ][ 'Schools_ID' ],
     );
   }
 
-  @Get('grades/:cmid')
-  findGradesAllByid(@Param('cmid') cmid: string) {
-    return this.controlMissionService.findGradesByCMID(+cmid);
+  @Get( 'grades/:cmid' )
+  findGradesAllByid ( @Param( 'cmid' ) cmid: string )
+  {
+    return this.controlMissionService.findGradesByCMID( +cmid );
   }
 
-  @Get('education-year/:educationYearId')
-  findAllByEducationYearId(@Param('educationYearId') educationYearId: string) {
+  @Get( 'education-year/:educationYearId' )
+  findAllByEducationYearId ( @Param( 'educationYearId' ) educationYearId: string )
+  {
     return this.controlMissionService.findAllByEducationYearId(
       +educationYearId,
     );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.controlMissionService.findOne(+id);
+  @Get( ':id' )
+  findOne ( @Param( 'id' ) id: string )
+  {
+    return this.controlMissionService.findOne( +id );
   }
 
-  @Roles(Role.SuperAdmin)
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
+  @Roles( Role.SuperAdmin )
+  @Patch( ':id' )
+  update (
+    @Param( 'id' ) id: string,
     @Body() updateControlMissionDto: UpdateControlMissionDto,
     @Req() req: Request,
-  ) {
+  )
+  {
     return this.controlMissionService.update(
       +id,
       updateControlMissionDto,
-      req.headers['user']['userId'],
+      req.headers[ 'user' ][ 'userId' ],
     );
   }
 
-  @Roles(Role.SuperAdmin)
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.controlMissionService.remove(+id);
+  @Roles( Role.SuperAdmin )
+  @Delete( ':id' )
+  remove ( @Param( 'id' ) id: string )
+  {
+    return this.controlMissionService.remove( +id );
   }
 
-  @Roles(Role.SuperAdmin)
-  @Patch('activate/:id')
-  activate(@Param('id') id: string, @Req() req: Request) {
+  @Roles( Role.SuperAdmin )
+  @Patch( 'activate/:id' )
+  activate ( @Param( 'id' ) id: string, @Req() req: Request )
+  {
     return this.controlMissionService.activate(
       +id,
-      req.headers['user']['userId'],
+      req.headers[ 'user' ][ 'userId' ],
     );
   }
 
-  @Roles(Role.SuperAdmin)
-  @Patch('deactivate/:id')
-  deactivate(@Param('id') id: string, @Req() req: Request) {
+  @Roles( Role.SuperAdmin )
+  @Patch( 'deactivate/:id' )
+  deactivate ( @Param( 'id' ) id: string, @Req() req: Request )
+  {
     return this.controlMissionService.deactivate(
       +id,
-      req.headers['user']['userId'],
+      req.headers[ 'user' ][ 'userId' ],
     );
   }
 }
