b5655090c58d8dfef199e38d8b2b39b8b28347be
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-31 11:21:45 +0300

    ✨ feat(auth): Enhance login and logout functionality
    - Add support for proctor login and active user check
    - Improve refresh token handling and generation
    - Implement logout functionality to remove refresh tokens
    - Refactor code for better readability and maintainability

--- src/Component/auth/auth.service.ts
+++ src/Component/auth/auth.service.ts
@@ -3,29 +3,34 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { sign, verify } from 'jsonwebtoken';
 import RefreshToken from './entities/refreshtoken.entities';
-var admin = require('firebase-admin');
+var admin = require( 'firebase-admin' );
 
-import {
+import
+{
   BadRequestException,
   NotFoundException,
 } from '@nestjs/common/exceptions';
 import { UsersService } from 'src/Users_System/users/users.service';
 
 @Injectable()
-export class AuthService {
+export class AuthService
+{
   private refreshTokens: RefreshToken[] = [];
-  constructor(
+  constructor (
     private readonly userServer: UsersService,
     private jwtService: JwtService,
   ) { }
 
-  async refresh(refreshStr: string): Promise<String | undefined> {
-    const refresToken = await this.retrieveRefreshToken(refreshStr);
-    if (!refresToken) {
+  async refresh ( refreshStr: string ): Promise<String | undefined>
+  {
+    const refresToken = await this.retrieveRefreshToken( refreshStr );
+    if ( !refresToken )
+    {
       return undefined;
     }
-    const user = await this.userServer.findOne(refresToken.userId);
-    if (!user) {
+    const user = await this.userServer.findOne( refresToken.userId );
+    if ( !user )
+    {
       return undefined;
     }
 
@@ -43,72 +48,85 @@ export class AuthService {
     );
   }
 
-  private retrieveRefreshToken(
+  private retrieveRefreshToken (
     refreshStr: string,
-  ): Promise<RefreshToken | undefined> {
-    try {
+  ): Promise<RefreshToken | undefined>
+  {
+    try
+    {
       // verify is imported from jsonwebtoken like import { sign, verify } from 'jsonwebtoken';
-      const decoded = verify(refreshStr, process.env.REFRESH_SECRET);
-      if (typeof decoded === 'string') {
+      const decoded = verify( refreshStr, process.env.REFRESH_SECRET );
+      if ( typeof decoded === 'string' )
+      {
         return undefined;
       }
       return Promise.resolve(
-        this.refreshTokens.find((token) => token.id === decoded.id),
+        this.refreshTokens.find( ( token ) => token.id === decoded.id ),
       );
-    } catch (e) {
+    } catch ( e )
+    {
       return undefined;
     }
   }
 
-  async login(
+  async login (
     userName: string,
     password: string,
     mobile: number,
-  ): Promise<{ accessToken: string; refreshToken: string } | undefined> {
-    if (mobile == 1) {
-      const user = await this.userServer.findOneProctorByUserName(userName);
-      if (!user) {
-        throw new BadRequestException('Procotr not found');
+  ): Promise<{ accessToken: string; refreshToken: string; } | undefined>
+  {
+    if ( mobile == 1 )
+    {
+      const user = await this.userServer.findOneProctorByUserName( userName );
+      if ( !user )
+      {
+        throw new BadRequestException( 'Procotr not found' );
       }
-      if (user.Password !== password) {
-        throw new BadRequestException('password is not right');
+      if ( user.Password !== password )
+      {
+        throw new BadRequestException( 'password is not right' );
       }
 
-      return this.newRefreshAndAccessToken(user, user.isFloorManager ??  'proctor');
-    } else {
-      const user = await this.userServer.findOneByUserName(userName);
-      if (!user) {
+      return this.newRefreshAndAccessToken( user, user.isFloorManager ?? 'proctor' );
+    } else
+    {
+      const user = await this.userServer.findOneByUserName( userName );
+      if ( !user )
+      {
         return undefined;
       }
-      if (user.Password !== password) {
+      if ( user.Password !== password )
+      {
         return undefined;
       }
-      if (user.Active == 1) {
-        return this.newRefreshAndAccessToken(user, 'user');
+      if ( user.Active == 1 )
+      {
+        return this.newRefreshAndAccessToken( user, 'user' );
       }
-      throw new BadRequestException('User is not active');
+      throw new BadRequestException( 'User is not active' );
     }
   }
 
-  private async newRefreshAndAccessToken(
+  private async newRefreshAndAccessToken (
     user: any,
     type: string
     // user: users,
     // values: { userAgent: string; ipAddress: string },
-  ): Promise<{ accessToken: string; refreshToken: string; userProfile: any }> {
-    const refreshObject = new RefreshToken({
+  ): Promise<{ accessToken: string; refreshToken: string; userProfile: any; }>
+  {
+    const refreshObject = new RefreshToken( {
       id:
         this.refreshTokens.length === 0
           ? 0
-          : this.refreshTokens[this.refreshTokens.length - 1].id + 1,
+          : this.refreshTokens[ this.refreshTokens.length - 1 ].id + 1,
       // ...values,
       schoolId: user.School_Id ?? user.LastSelectSchoolId,
       userId: user.ID,
-      roles: user.Roles == undefined ? [] : user.Roles.map((role) => role.Name),
+      roles: user.Roles == undefined ? [] : user.Roles.map( ( role ) => role.Name ),
       type: type
-    });
+    } );
     // add refreshObject to your db in real app
-    this.refreshTokens.push(refreshObject);
+    this.refreshTokens.push( refreshObject );
 
     return {
       refreshToken: refreshObject.sign(),
@@ -118,7 +136,7 @@ export class AuthService {
           userId: user.ID,
           schoolId: user.School_Id ?? user.LastSelectSchoolId,
           roles:
-            user.Roles == undefined ? [] : user.Roles.map((role) => role.Name),
+            user.Roles == undefined ? [] : user.Roles.map( ( role ) => role.Name ),
           type: type
         },
         process.env.ACCESS_SECRET,
@@ -130,16 +148,18 @@ export class AuthService {
     };
   }
 
-  async logout(refreshStr) {
-    const refreshToken = await this.retrieveRefreshToken(refreshStr);
+  async logout ( refreshStr )
+  {
+    const refreshToken = await this.retrieveRefreshToken( refreshStr );
 
-    if (!refreshToken) {
+    if ( !refreshToken )
+    {
       return;
     }
     // delete refreshToken From db
 
     this.refreshTokens = this.refreshTokens.filter(
-      (refreshToken: RefreshToken) => refreshToken.id !== refreshToken.id,
+      ( refreshToken: RefreshToken ) => refreshToken.id !== refreshToken.id,
     );
   }
 
@@ -154,40 +174,46 @@ export class AuthService {
   //   console.log(user);
   // }
 
-  async studentLoginForExam(
-    email,
-    password,
-  ): Promise<{ accessToken: string; refreshToken: string } | undefined> {
-    const student = await this.userServer.findOneStudentByUserName(email);
-    if (!student) {
-      throw new NotFoundException('student not found');
+  async studentLoginForExam (
+    userName: string,
+    password: string,
+  ): Promise<{ accessToken: string; refreshToken: string; } | undefined>
+  {
+    const student = await this.userServer.findOneStudentByUserName( userName );
+    if ( !student )
+    {
+      throw new NotFoundException( 'student not found' );
     }
-    if (student.Password !== password) {
-      throw new BadRequestException('password is not right');
+    if ( student.Password !== password )
+    {
+      throw new BadRequestException( 'password is not right' );
     }
-    return this.newRefreshAndAccessToken(student, 'student');
+    return this.newRefreshAndAccessToken( student, 'student' );
   }
 
-  async validateRequest(request: any) {
+  async validateRequest ( request: any )
+  {
     if (
-      request.headers['authorization'] &&
-      request.headers['authorization'].split(' ')[0] === 'Bearer' &&
-      (await this.jwtService.decode(
-        request.headers['authorization'].split(' ')[1],
-      ))
-    ) {
+      request.headers[ 'authorization' ] &&
+      request.headers[ 'authorization' ].split( ' ' )[ 0 ] === 'Bearer' &&
+      ( await this.jwtService.decode(
+        request.headers[ 'authorization' ].split( ' ' )[ 1 ],
+      ) )
+    )
+    {
       var decodedToken = this.jwtService.decode(
-        request.headers['authorization'].split(' ')[1],
+        request.headers[ 'authorization' ].split( ' ' )[ 1 ],
       );
-      console.log('Decoded Token:', decodedToken);
-      request.headers['user'] = {};
-      request.headers['user']['userId'] = decodedToken.userId;
-      request.headers['user']['roles'] = decodedToken.roles;
-      request.headers['user']['schoolId'] = decodedToken.schoolId;
-      request.headers['user']['type'] = decodedToken.type;
+      console.log( 'Decoded Token:', decodedToken );
+      request.headers[ 'user' ] = {};
+      request.headers[ 'user' ][ 'userId' ] = decodedToken.userId;
+      request.headers[ 'user' ][ 'roles' ] = decodedToken.roles;
+      request.headers[ 'user' ][ 'schoolId' ] = decodedToken.schoolId;
+      request.headers[ 'user' ][ 'type' ] = decodedToken.type;
 
       return true;
-    } else {
+    } else
+    {
       return true;
     }
   }
