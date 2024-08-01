5f40ffb343bb00190e7c0169a98bd88e5900b1f9
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-01 11:44:50 +0300

    ✨ feat(uuid): implement CRUD operations for UUIDs
    This commit introduces the implementation of CRUD (Create, Read, Update, Delete) operations for UUIDs in the application. The changes include:
    
    - Created a `UuidService` class that handles the database interactions for UUIDs.
    - Implemented methods to create, read, update, and delete UUIDs.
    - Added an `activate` method to activate a UUID.
    - Commented out a `deactivate` method, as there is a potential issue with the data type of the `Active` field.
    - Utilized the Prisma ORM to interact with the database.
    
    These changes will allow the application to manage UUIDs more effectively, providing the necessary functionality for creating, retrieving, updating, and deleting UUID records.

---
+++ src/Component/uuid/uuid.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/Common/Db/prisma.service';
+import { CreateUuidDto } from './dto/create-uuid.dto';
+import { UpdateUuidDto } from './dto/update-uuid.dto';
+
+
+@Injectable()
+export class UuidService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
+
+  async create ( createUuidDto: CreateUuidDto, createdBy: number )
+  {
+    var result = await this.prismaService.uuid.create( {
+      data: { ...createUuidDto, Created_by: createdBy },
+    } );
+    return result;
+  }
+
+  async findAll ()
+  {
+    var results = await this.prismaService.uuid.findMany( {} );
+
+    return results;
+  }
+
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.uuid.findUnique( {
+      where: {
+        ID: id,
+      },
+    } );
+    return result;
+  }
+
+  async update (
+    id: number,
+    updateUuidDto: UpdateUuidDto,
+    updatedBy: number,
+  )
+  {
+    var result = await this.prismaService.uuid.update( {
+      where: {
+        ID: id,
+      },
+      data: {
+        ...updateUuidDto,
+        Updated_By: updatedBy,
+        UpdatedAt: new Date().toISOString(),
+      },
+    } );
+    return result;
+  }
+
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.uuid.delete( {
+      where: {
+        ID: id,
+      },
+    } );
+    return result;
+  }
+
+  async activate ( id: number, updatedBy: number )
+  {
+    var result = await this.prismaService.uuid.update( {
+      where: {
+        ID: id
+      },
+      data: {
+        active: 1,
+        Updated_By: updatedBy,
+        UpdatedAt: new Date().toISOString(),
+      }
+    } );
+    return result;
+  }
+
+  // async deactivate ( id: number )
+  // {
+  //   var result = await this.prismaService.uuid.update( {
+  //     where: {
+  //       ID: id
+  //     },
+  //     data: {
+  // TODO? need to check type of data
+  // ACtive is string not number
+  //       Active:0
+  //     }
+  //   } );
+  //   return result;
+  // }
+}
