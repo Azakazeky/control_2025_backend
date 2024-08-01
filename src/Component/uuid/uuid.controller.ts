954cc6aaf050da45ac025e2f361f4bac4eb6bbf3
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-01 11:45:10 +0300

    ✨ feat: Implement UUID CRUD operations
    Adds a new `UuidController` that provides endpoints for creating, reading,
    updating, and deleting UUIDs. The controller is protected by the
    `JwtAuthGuard`, ensuring only authenticated users can access these
    endpoints.
    
    The main operations include:
    
    - Creating a new UUID with a `CreateUuidDto`
    - Fetching all UUIDs
    - Retrieving a specific UUID by its ID
    - Updating a UUID's details with an `UpdateUuidDto`
    - Activating a UUID
    - Removing a UUID
    
    These changes enable the management of UUIDs within the application.

---
+++ src/Component/uuid/uuid.controller.ts
@@ -0,0 +1,75 @@
+import
+{
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
+import { CreateUuidDto } from './dto/create-uuid.dto';
+import { UpdateUuidDto } from './dto/update-uuid.dto';
+import { UuidService } from './uuid.service';
+
+@UseGuards( JwtAuthGuard )
+@ApiTags( 'Uuid' )
+@Controller( 'uuid' )
+export class UuidController
+{
+  constructor ( private readonly uuidService: UuidService ) { }
+  @Post()
+  create ( @Body() createUuidDto: CreateUuidDto, @Req() req: Request )
+  {
+    return this.uuidService.create(
+      createUuidDto,
+      req.headers[ 'user' ][ 'userId' ],
+    );
+  }
+
+  @Get()
+  findAll ()
+  {
+    return this.uuidService.findAll();
+  }
+
+  @Get( ':id' )
+  findOne ( @Param( 'id' ) id: string )
+  {
+    return this.uuidService.findOne( +id );
+  }
+  @Patch( ':id' )
+  update (
+    @Param( 'id' ) id: string,
+    @Body() updateUuidDto: UpdateUuidDto,
+    @Req() req: Request,
+  )
+  {
+    return this.uuidService.update(
+      +id,
+      updateUuidDto,
+      req.headers[ 'user' ][ 'userId' ],
+    );
+  }
+
+  @Patch( ':id/active' )
+  active (
+    @Param( 'id' ) id: string,
+    @Req() req: Request,
+  )
+  {
+    return this.uuidService.activate(
+      +id,
+      +req.headers[ 'user' ][ 'userId' ],
+    );
+  }
+  @Delete( ':id' )
+  remove ( @Param( 'id' ) id: string )
+  {
+    return this.uuidService.remove( +id );
+  }
+}
