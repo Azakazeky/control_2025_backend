cdf9583b6f925e74b906286db55067136fad7fee
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-06 15:20:20 +0300

    🚧 feat(subjects): Enhance subject management
    This commit introduces several improvements to the `SubjectsService` in the
    school setting component:
    
    1. The `create` method now supports creating a subject and associating it
       with multiple school types.
    2. The `update` method now handles updating the subject's details as well
       as its associations with school types.
    3. A new `connectSubjectToSchoolType` method has been added to connect a
       subject to a specific school type.
    4. The `findAllBySchoolTypeId` method has been optimized to fetch subjects
       based on the provided school type ID.
    5. Minor code formatting and style improvements have been made throughout
       the file.
    
    These changes enhance the functionality and flexibility of the subjects
    management feature, allowing more seamless integration with school type
    associations.

--- src/Component/School_Setting/subjects/subjects.service.ts
+++ src/Component/School_Setting/subjects/subjects.service.ts
@@ -4,24 +4,42 @@ import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
 
 @Injectable()
-export class SubjectsService {
-  constructor(private readonly prismaService: PrismaService) { }
+export class SubjectsService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
 
-  async create(createSubjecteDto: CreateSubjectDto, createdBy: number) {
-    var result = await this.prismaService.subjects.create({
-      data: { ...createSubjecteDto, Created_By: createdBy },
-    });
+  async create ( createSubjecteDto: CreateSubjectDto, createdBy: number )
+  {
+    var result = await this.prismaService.subjects.create( {
+      data: {
+        Name: createSubjecteDto.Name,
+        Active: createSubjecteDto.Active,
+        Created_By: createdBy,
+        InExam: createSubjecteDto.InExam,
+      }
+    } );
+    createSubjecteDto.schools_type_ID.map( async ( id ) =>
+    {
+      await this.prismaService.school_type_has_subjects.create( {
+        data: {
+          school_type_ID: id,
+          subjects_ID: result.ID,
+        },
+      } );
+    } );
     return result;
   }
 
-  async findAll() {
-    var results = await this.prismaService.subjects.findMany({});
+  async findAll ()
+  {
+    var results = await this.prismaService.subjects.findMany( {} );
 
     return results;
   }
 
-  async findAllBySchoolTypeId(school_type_ID: number) {
-    var results = await this.prismaService.subjects.findMany({
+  async findAllBySchoolTypeId ( school_type_ID: number )
+  {
+    var results = await this.prismaService.subjects.findMany( {
       where: {
         school_type_has_subjects: {
           every: {
@@ -29,55 +47,88 @@ export class SubjectsService {
           }
         }
       }
-    });
-    return results.map((obj) => obj);
+    } );
+    return results.map( ( obj ) => obj );
   }
 
-  async findOne(id: number) {
-    var result = await this.prismaService.subjects.findUnique({
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.subjects.findUnique( {
       where: {
         ID: id,
       },
-    });
+    } );
     return result;
   }
 
-  async update(
+  async update (
     id: number,
     updateSubjecteDto: UpdateSubjectDto,
     updatedBy: number,
-  ) {
-    var result = await this.prismaService.subjects.update({
+  )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id,
       },
       data: {
-        ...updateSubjecteDto,
+        Active: updateSubjecteDto.Active,
+        Name: updateSubjecteDto.Name,
+        InExam: updateSubjecteDto.InExam,
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       },
-    });
+    } );
+
+    await this.prismaService.school_type_has_subjects.deleteMany( {
+      where: {
+        subjects_ID: id,
+      },
+    } );
+
+    updateSubjecteDto.schools_type_ID.map( async ( id ) =>
+    {
+      await this.prismaService.school_type_has_subjects.create( {
+        data: {
+          school_type_ID: id,
+          subjects_ID: result.ID,
+        },
+      } );
+    } );
     return result;
   }
 
-  async remove(id: number) {
-    var result = await this.prismaService.subjects.delete({
+  async connectSubjectToSchoolType ( id: number, school_type_ID: number )
+  {
+    var result = await this.prismaService.school_type_has_subjects.create( {
+      data: {
+        school_type_ID,
+        subjects_ID: id,
+      },
+    } );
+    return result;
+  }
+
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.subjects.delete( {
       where: {
         ID: id,
       },
-    });
+    } );
     return result;
   }
 
 
 
 
-  async chageInExamState(
+  async chageInExamState (
     id: number,
     number: number,
     updatedBy: number,
-  ) {
-    var result = await this.prismaService.subjects.update({
+  )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id,
       },
@@ -86,15 +137,16 @@ export class SubjectsService {
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       },
-    });
+    } );
     return result;
   }
-  async reactive(
+  async reactive (
     id: number,
     active: number,
     updatedBy: number,
-  ) {
-    var result = await this.prismaService.subjects.update({
+  )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id,
       },
@@ -103,17 +155,18 @@ export class SubjectsService {
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       },
-    });
+    } );
     return result;
   }
 
 
-  async deactive(
+  async deactive (
     id: number,
 
     active: number, updatedBy: number,
-  ) {
-    var result = await this.prismaService.subjects.update({
+  )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id,
       },
@@ -122,7 +175,7 @@ export class SubjectsService {
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       },
-    });
+    } );
     return result;
   }
 
