1f01e07c9ce1892403e784bc6dcae82e7345197a
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-06 15:20:11 +0300

    🚀 feat: Add school_type_ID array to CreateSubjectDto
    Adds a new field `schools_type_ID` of type `number[]` to the `CreateSubjectDto`
    class. This allows for associating a subject with multiple school types during
    creation. The validation rules have also been updated to ensure the new field
    is an array of numbers.

--- src/Component/School_Setting/subjects/dto/create-subject.dto.ts
+++ src/Component/School_Setting/subjects/dto/create-subject.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 
-export class CreateSubjectDto {
+export class CreateSubjectDto
+{
   @ApiProperty()
   @IsString()
   Name: string;
 
   Active?: number;
 
-  InExam?: number
+  InExam?: number;
+
+  @ApiProperty( { type: Number, isArray: true } )
+  @IsArray( { each: true } )
+  schools_type_ID: number[];
 }
