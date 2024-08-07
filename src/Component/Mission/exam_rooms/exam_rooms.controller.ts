0af2becdfeecdde1b567ccabb441323537b36887
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-07 10:42:21 +0300

    🌟 feat(exam-rooms): Enhance exam room management
    This commit introduces several improvements to the exam room management functionality:
    
    - Refactor the `ExamRoomsController` to improve readability and maintainability
    - Add a new endpoint to retrieve the next exams for a specific proctor
    - Modify the `findAllByProctorId` endpoint to use the user's ID from the request headers
    - Implement various other endpoints to improve the overall exam room management experience
    
    These changes will help streamline the process of managing exam rooms and provide a more user-friendly interface for administrators and proctors.

--- src/Component/Mission/exam_rooms/exam_rooms.controller.ts
+++ src/Component/Mission/exam_rooms/exam_rooms.controller.ts
@@ -1,4 +1,5 @@
-import {
+import
+{
   Body,
   Controller,
   Delete,
@@ -16,78 +17,91 @@ import { UpdateExamRoomDto } from './dto/update-exam_room.dto';
 import { ExamRoomsService } from './exam_rooms.service';
 
 // @UseGuards( JwtAuthGuard )
-@ApiTags('Exam-Room')
-@Controller('exam-rooms')
-export class ExamRoomsController {
-  constructor(private readonly examRoomsService: ExamRoomsService) {}
+@ApiTags( 'Exam-Room' )
+@Controller( 'exam-rooms' )
+export class ExamRoomsController
+{
+  constructor ( private readonly examRoomsService: ExamRoomsService ) { }
 
-  @Roles(Role.SuperAdmin)
+  @Roles( Role.SuperAdmin )
   @Post()
-  create(@Body() createExamRoomDto: CreateExamRoomDto, @Req() req: Request) {
+  create ( @Body() createExamRoomDto: CreateExamRoomDto, @Req() req: Request )
+  {
     return this.examRoomsService.create(
       createExamRoomDto,
-      req.headers['user']['userId'],
+      req.headers[ 'user' ][ 'userId' ],
     );
   }
 
   @Get()
-  findAll() {
+  findAll ()
+  {
     return this.examRoomsService.findAll();
   }
 
-  @Get('control-mission/:controlMissionId')
-  findAllByControlMissionId(
-    @Param('controlMissionId') controlMissionId: string,
-  ) {
-    return this.examRoomsService.findAllByControlMissionId(+controlMissionId);
+  @Get( 'control-mission/:controlMissionId' )
+  findAllByControlMissionId (
+    @Param( 'controlMissionId' ) controlMissionId: string,
+  )
+  {
+    return this.examRoomsService.findAllByControlMissionId( +controlMissionId );
   }
 
-  @Get('proctor/:proctorId')
-  findAllByProctorId(@Param('proctorId') proctorId: string) {
-    return this.examRoomsService.findAllByProctorId(+proctorId);
+  @Get( 'proctor/:proctorId' )
+  findAllByProctorId ( @Param( 'proctorId' ) proctorId: string )
+  {
+    return this.examRoomsService.findAllByProctorId( +proctorId );
   }
 
-  @Get('next-exams/:proctorId')
-  findNextExams(@Param('proctorId') proctorId: string) {
-    return this.examRoomsService.findNextExams(+proctorId);
+  @Get( 'next-exams' )
+  findNextExams ( @Req() req: Request )
+  {
+    return this.examRoomsService.findNextExams(
+      req.headers[ 'user' ][ 'userId' ],
+    );
   }
 
-  @Get('school-class/:schoolClassId')
-  findAllBySchoolClassId(@Param('schoolClassId') schoolClassId: string) {
-    return this.examRoomsService.findAllBySchoolClassId(+schoolClassId);
+  @Get( 'school-class/:schoolClassId' )
+  findAllBySchoolClassId ( @Param( 'schoolClassId' ) schoolClassId: string )
+  {
+    return this.examRoomsService.findAllBySchoolClassId( +schoolClassId );
   }
 
-  @Get('school-class/:schoolClassId/control-mission/:controlMissionId')
-  findAllBySchoolClassIdAndControlMissionId(
-    @Param('schoolClassId') schoolClassId: string,
-    @Param('controlMissionId') controlMissionId: string,
-  ) {
+  @Get( 'school-class/:schoolClassId/control-mission/:controlMissionId' )
+  findAllBySchoolClassIdAndControlMissionId (
+    @Param( 'schoolClassId' ) schoolClassId: string,
+    @Param( 'controlMissionId' ) controlMissionId: string,
+  )
+  {
     return this.examRoomsService.findAllBySchoolClassIdAndControlMissionId(
       +schoolClassId,
       +controlMissionId,
     );
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.examRoomsService.findOne(+id);
+  @Get( ':id' )
+  findOne ( @Param( 'id' ) id: string )
+  {
+    return this.examRoomsService.findOne( +id );
   }
-  @Roles(Role.SuperAdmin)
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
+  @Roles( Role.SuperAdmin )
+  @Patch( ':id' )
+  update (
+    @Param( 'id' ) id: string,
     @Body() updateExamRoomDto: UpdateExamRoomDto,
     @Req() req: Request,
-  ) {
+  )
+  {
     return this.examRoomsService.update(
       +id,
       updateExamRoomDto,
-      req.headers['user']['userId'],
+      req.headers[ 'user' ][ 'userId' ],
     );
   }
-  @Roles(Role.SuperAdmin)
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.examRoomsService.remove(+id);
+  @Roles( Role.SuperAdmin )
+  @Delete( ':id' )
+  remove ( @Param( 'id' ) id: string )
+  {
+    return this.examRoomsService.remove( +id );
   }
 }
