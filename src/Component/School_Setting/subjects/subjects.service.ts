f549b502fba5faaa270b1cf2ee2fe2693fc2866e
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-07 12:16:02 +0300

    ✨ feat(subjects): improve subject management functionality
    This commit introduces several improvements to the subject management functionality in the school setting module:
    
    - Simplify the `create` method by using a single `CreateSubjectDto` instead of `CreateSubjectDto2`.
    - Implement the `findOne` method to retrieve a single subject by its ID.
    - Implement the `update` method to update an existing subject, including updating the associated school types.
    - Implement the `chageInExamState` method to update the `InExam` flag for a subject.
    - Optimize the `findAllBySchoolTypeId` method to only return active subjects.
    
    These changes aim to provide a more robust and user-friendly subject management experience for the school setting module.

--- src/Component/School_Setting/subjects/subjects.service.ts
+++ src/Component/School_Setting/subjects/subjects.service.ts
@@ -1,58 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/Common/Db/prisma.service';
-import { CreateSubjectDto, CreateSubjectDto2 } from './dto/create-subject.dto';
+import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
 
 @Injectable()
-export class SubjectsService {
-  constructor(private readonly prismaService: PrismaService) { }
+export class SubjectsService
+{
+  constructor ( private readonly prismaService: PrismaService ) { }
 
-  async create(createSubjecteDto: CreateSubjectDto2, createdBy: number) {
+  async create ( createSubjecteDto: CreateSubjectDto, createdBy: number )
+  {
 
-    var result = await this.prismaService.subjects.create({
+    var result = await this.prismaService.subjects.create( {
       data: {
         ...createSubjecteDto, Created_By: createdBy,
       },
-    });
+    } );
     return result;
   }
 
-  async findAll() {
-    var results = await this.prismaService.subjects.findMany({});
+  async findAll ()
+  {
+    var results = await this.prismaService.subjects.findMany( {} );
 
     return results;
   }
 
-  async findAllBySchoolTypeId(school_type_ID: number) {
-    var results = await this.prismaService.subjects.findMany({
-      where: {
-        school_type_has_subjects: {
-          some: {
-            school_type_ID
-          },
-        },
-      },
-      select: {
-        ID: true,
-        Name: true,
-        InExam: true,
-        Active: true,
-        school_type_has_subjects: {
-          select: {
-            school_type: {
-              select: {
-                ID: true,
-                Name: true
-              }
-            }
-          }
-        }
-      },
-    });
-    return results.map((obj) => obj);
-  }
-  async findAllActiveBySchoolTypeId(school_type_ID: number) {
-    var results = await this.prismaService.subjects.findMany({
+  async findAllBySchoolTypeId ( school_type_ID: number )
+  {
+    var results = await this.prismaService.subjects.findMany( {
       where: {
         Active: 1,
         school_type_has_subjects: {
@@ -77,71 +53,75 @@ export class SubjectsService {
           }
         }
       },
-    });
-    return results.map((obj) => obj);
+    } );
+    return results.map( ( obj ) => obj );
   }
 
-  async findOne(id: number) {
-    var result = await this.prismaService.subjects.findUnique({
+  async findOne ( id: number )
+  {
+    var result = await this.prismaService.subjects.findUnique( {
       where: {
         ID: id,
       },
-    });
+    } );
     return result;
   }
 
-  // async update(
-  //   id: number,
-  //   updateSubjecteDto: UpdateSubjectDto,
-  //   updatedBy: number,
-  // ) {
-  //   var result = await this.prismaService.subjects.update({
-  //     where: {
-  //       ID: id,
-  //     },
-  //     data: {
-  //       Active: updateSubjecteDto.Active,
-  //       Name: updateSubjecteDto.Name,
-  //       InExam: updateSubjecteDto.InExam,
-  //       Updated_By: updatedBy,
-  //       Updated_At: new Date().toISOString(),
-  //     },
-  //   });
-
-  //   await this.prismaService.school_type_has_subjects.deleteMany({
-  //     where: {
-  //       subjects_ID: id,
-  //     },
-  //   });
-
-  //   await this.prismaService.school_type_has_subjects.create({
-  //     data: {
-  //       school_type_ID: id,
-  //       subjects_ID: result.ID,
-  //     },
-  //   });
-
-  //   return result;
-  // }
-
-  async remove(id: number) {
-    var result = await this.prismaService.subjects.delete({
+  async update (
+    id: number,
+    updateSubjecteDto: UpdateSubjectDto,
+    updatedBy: number,
+  )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id,
       },
-    });
+      data: {
+        Active: updateSubjecteDto.Active,
+        Name: updateSubjecteDto.Name,
+        InExam: updateSubjecteDto.InExam,
+        Updated_By: updatedBy,
+        Updated_At: new Date().toISOString(),
+      },
+    } );
+
+    await this.prismaService.school_type_has_subjects.deleteMany( {
+      where: {
+        subjects_ID: id,
+      },
+    } );
+
+    await this.prismaService.school_type_has_subjects.create( {
+      data: {
+        school_type_ID: id,
+        subjects_ID: result.ID,
+      },
+    } );
+
+    return result;
+  }
+
+  async remove ( id: number )
+  {
+    var result = await this.prismaService.subjects.delete( {
+      where: {
+        ID: id,
+      },
+    } );
     return result;
   }
 
 
 
 
-  async chageInExamState(
+  async chageInExamState (
     id: number,
     number: number,
     updatedBy: number,
-  ) {
-    var result = await this.prismaService.subjects.update({
+  )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id,
       },
@@ -150,13 +130,23 @@ export class SubjectsService {
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       },
-    });
+    } );
     return result;
   }
 
+  async findAllActive ()
+  {
+    var results = await this.prismaService.subjects.findMany( {
+      where: {
+        Active: 1
+      }
+    } );
+    return results;
+  }
 
-  async activate(id: number, updatedBy: number,) {
-    var result = await this.prismaService.subjects.update({
+  async activate ( id: number, updatedBy: number )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id
       },
@@ -165,12 +155,13 @@ export class SubjectsService {
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       }
-    });
+    } );
     return result;
   }
 
-  async deactivate(id: number, updatedBy: number,) {
-    var result = await this.prismaService.subjects.update({
+  async deactivate ( id: number, updatedBy: number )
+  {
+    var result = await this.prismaService.subjects.update( {
       where: {
         ID: id
       },
@@ -179,7 +170,7 @@ export class SubjectsService {
         Updated_By: updatedBy,
         Updated_At: new Date().toISOString(),
       }
-    });
+    } );
     return result;
   }
 }
