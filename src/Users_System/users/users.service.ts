565a23cbcfbe6c8a81314d62fb9df1a3a1598e95
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-08 12:25:34 +0300

    ✨ feat(users): add findAllCreatedBy method and optimize findAll
    Adds a new method `findAllCreatedBy` to the `UsersService` that allows
    filtering users by the `Created_By` field. Also optimizes the `findAll`
    method to only return the necessary fields, improving performance.

--- src/Users_System/users/users.service.ts
+++ src/Users_System/users/users.service.ts
@@ -57,7 +57,28 @@ export class UsersService
 
   async findAll ()
   {
-    var results = await this.prismaService.users.findMany( {} );
+    var results = await this.prismaService.users.findMany( {
+      select: {
+        ID: true,
+        Active: true,
+        Created_At: true,
+        Created_By: true,
+        Full_Name: true,
+        Type: true,
+        User_Name: true,
+      }
+    } );
+
+    return results;
+  }
+
+  async findAllCreatedBy ( createdBy: number )
+  {
+    var results = await this.prismaService.users.findMany( {
+      where: {
+        Created_By: createdBy,
+      },
+    } );
 
     return results;
   }
