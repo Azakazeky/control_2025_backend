437e0e4e659fa77c6ab1e423f1ab295073a0d170
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-08-11 08:36:48 +0300

    success push production

--- src/Component/auth/startgy/jwt.strategy.ts
+++ src/Component/auth/startgy/jwt.strategy.ts
@@ -6,7 +6,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.ACCESS_SECRET,
+      secretOrKey:'C2287E7F65DB21F3',
+      // process.env.ACCESS_SECRET,
     });
   }
 
