dfef46695072bea1553b7dd1932ae90836f438cb
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-12 11:36:05 +0300

    💡 feat(users): add password update fields to UpdateUserDto
    Adds the `OldPassword` and `NewPassword` fields to the `UpdateUserDto` class to
    enable password update functionality. This change is necessary to allow users
    to update their passwords through the API.

--- src/Users_System/users/dto/update-user.dto.ts
+++ src/Users_System/users/dto/update-user.dto.ts
@@ -1,6 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { CreateUserDto, CreateUserHasRolesDto, CreateUserHasSchoolsDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType( CreateUserDto ) { }
+export class UpdateUserDto extends PartialType( CreateUserDto ) {
+
+    @ApiProperty()
+    @IsString()
+    OldPassword: string;
+    @ApiProperty()
+    @IsString()
+    NewPassword: string;
+}
 export class UpdateUserHasRolesDto extends PartialType( CreateUserHasRolesDto ) { }
 export class UpdateUserHasSchoolsDto extends PartialType( CreateUserHasSchoolsDto ) { }
