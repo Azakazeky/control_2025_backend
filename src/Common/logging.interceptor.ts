9ba4ea3407b7088e93c147ec458cc5e23114878a
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-09-09 14:19:46 +0300

    looger v2

--- src/Common/logging.interceptor.ts
+++ src/Common/logging.interceptor.ts
@@ -7,43 +7,52 @@ import {
 } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { Observable, catchError, map, of } from 'rxjs';
+import { PrismaService } from './Db/prisma.service';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  // constructor(private readonly PrismaService: PrismaService,) { }
+  constructor(private readonly PrismaService: PrismaService,) { }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    /*  let request=  context.switchToHttp().getRequest();
-     
-      this.PrismaService.systemlogger.create({
+    let req = context.switchToHttp().getRequest();
+
+    next.handle().forEach(async (value) => {
+      console.log(value);
+
+      console.log(req.body);
+
+      await this.PrismaService.system_logger.create({
         data: {
-          UserId:+request.user.userId,
-          JsonData: request.body,
-          TableName:'TableName',
-          Action:'Action',
-          CreatedDate:new Date().toISOString(),
+          Action: req.method,
+          UserId: req.headers['user'] == undefined ? "LOGIN" : '' + req.headers['user']['userId'] ?? 'no id',
+          TableName: req.url,
+          Record_Befor: req.body == undefined ? null : req.body.toString(),
+          Record_After: value.toString(),
         }
       });
-      */
 
-    return next.handle().pipe(
-      map((data) => ({
-        status: true,
-        // statusCode: context.switchToHttp().getResponse().statusCode,
-        message: 'Data has been get success',
-        data: data,
-      })),
-      catchError((err) => {
-        if (err instanceof HttpException) {
-          throw err;
-        } else if (err instanceof PrismaClientKnownRequestError) {
-          throw err;
-        }
-        return of({
-          status: false,
-          message: err.message,
-        });
-      }),
-    );
+    });
+
+
+    return next.handle()
+      .pipe(
+        map((data) => ({
+          status: true,
+          // statusCode: context.switchToHttp().getResponse().statusCode,
+          message: 'Data has been get success',
+          data: data,
+        })),
+        catchError((err) => {
+          if (err instanceof HttpException) {
+            throw err;
+          } else if (err instanceof PrismaClientKnownRequestError) {
+            throw err;
+          }
+          return of({
+            status: false,
+            message: err.message,
+          });
+        }),
+      );
   }
 }
