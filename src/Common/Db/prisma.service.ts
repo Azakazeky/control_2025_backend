e6528301607a7826ac4db072f2792928dc84d060
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-09-10 08:00:02 +0300

    🔧 refactor(prisma.service): remove unnecessary logger
    The changes made in this commit are:
    
    - Removed the unnecessary `LoggerPrismaClient` extension, which was
      responsible for logging Prisma operations. This was not being used
      and added unnecessary complexity to the code.
    - Simplified the `PrismaService` class by removing the unnecessary
      object assignment.

--- src/Common/Db/prisma.service.ts
+++ src/Common/Db/prisma.service.ts
@@ -1,15 +1,9 @@
-import {
-  INestApplication,
-  Injectable,
-  Logger,
-  OnModuleInit,
-} from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    Object.assign(this, LoggerPrismaClient);
     await this.$connect();
   }
 
@@ -20,34 +14,3 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     // });
   }
 }
-
-export const LoggerPrismaClient = new PrismaClient().$extends({
-  query: {
-    $allModels: {
-      $allOperations: async ({ operation, args, query, model }) => {
-        Logger.log('userId', global.userId);
-        var result = await query(args);
-        if (
-          operation === 'create' ||
-          operation === 'update' ||
-          operation === 'delete' ||
-          operation === 'createMany' ||
-          operation === 'updateMany' ||
-          operation === 'deleteMany'
-        ) {
-          console.log(
-            'operation',
-            operation,
-            'args',
-            args,
-            'model',
-            model,
-            'result',
-            result,
-          );
-        }
-        return result;
-      },
-    },
-  },
-});
