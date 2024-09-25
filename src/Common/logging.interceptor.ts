6f27ef7144f880e251e85dd83632ecf92a9fb621
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-09-25 14:31:33 +0300

    edit logging interceptor

--- src/Common/logging.interceptor.ts
+++ src/Common/logging.interceptor.ts
@@ -11,27 +11,49 @@ import { PrismaService } from './Db/prisma.service';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  constructor(private readonly PrismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     let req = context.switchToHttp().getRequest();
 
     return next.handle().pipe(
       map(async (data) => {
-        // if (!req.url.includes('login')) {
-        // this.PrismaService.system_logger.create({
-        //   data: {
-        //     Action: req.method,
-        //     UserId:
-        //       req.headers['user'] == undefined
-        //         ? 'LOGIN'
-        //         : '' + req.headers['user']['userId'] ?? 'no id',
-        //     TableName: req.url,
-        //     Record_Befor: JSON.stringify(req.body),
-        //     Record_After: JSON.stringify(data),
-        //   },
-        // });
-        // }
+        if (
+          !(
+            req.url.includes('login') ||
+            req.url.includes('logout') ||
+            req.url.includes('refresh') ||
+            req.url.includes('token')
+          ) &&
+          !req.method.includes('GET')
+        ) {
+          console.log({
+            host: req.headers.host,
+            url: req.url,
+            platform: req.headers['sec-ch-ua-platform'],
+            userAgent: req.headers['user-agent'],
+            method: req.method.includes('POST')
+              ? 'Create'
+              : req.method.includes('PATCH') || req.method.includes('PUT')
+              ? 'Update'
+              : 'Delete',
+            body: JSON.stringify(req.body),
+            userId: req.headers['user']['userId'],
+          });
+
+          // this.PrismaService.system_logger.create({
+          //   data: {
+          //     Action: req.method,
+          //     UserId:
+          //       req.headers['user'] == undefined
+          //         ? 'LOGIN'
+          //         : '' + req.headers['user']['userId'] ?? 'no id':
+          //     TableName: req.url,
+          //     Record_Befor: JSON.stringify(req.body),
+          //     Record_After: JSON.stringify(data),
+          //   },
+          // });
+        }
         return {
           status: true,
           // statusCode: context.switchToHttp().getResponse().statusCode,
