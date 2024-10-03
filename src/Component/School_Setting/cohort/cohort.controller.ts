086519512540a1d17f99eb120daa6238a54a52c6
Abdelrahman Elzakazeky <42267852+Azakazeky@users.noreply.github.com>
2024-10-03 15:23:18 +0300

    upload cohrots from elsx

--- src/Component/School_Setting/cohort/cohort.controller.ts
+++ src/Component/School_Setting/cohort/cohort.controller.ts
@@ -4,26 +4,30 @@ import {
   Delete,
   Get,
   Param,
+  ParseFilePipeBuilder,
   Patch,
   Post,
   Req,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/Common/Guard/local-auth.guard';
 import Role from 'src/Common/Guard/role.enum';
 import { Roles } from 'src/Common/Guard/roles.decorator';
 import { CohortService } from './cohort.service';
 import { AddSubjectsToCohort, CreateCohortDto } from './dto/create-cohort.dto';
 import { UpdateCohortDto } from './dto/update-cohort.dto';
-
+import { diskStorage } from 'multer';
+import { File, FileInterceptor } from '@nest-lab/fastify-multer';
 let XLSX = require('xlsx');
 
 @UseGuards(JwtAuthGuard)
 @ApiTags('cohort')
 @Controller('cohort')
 export class CohortController {
-  constructor(private readonly cohortService: CohortService) {}
+  constructor(private readonly cohortService: CohortService) { }
   @Roles(Role.SuperAdmin, Role.ControlOfficer, Role.OperationCO)
   @Post()
   create(@Body() createCohortDto: CreateCohortDto, @Req() req: Request) {
@@ -115,110 +119,123 @@ export class CohortController {
     return this.cohortService.deactivate(+id);
   }
 
-  // @ApiConsumes("multipart/form-data")
-  // @ApiBody({
-  //   schema: {
-  //     type: "object",
-  //     properties: {
-  //       file: {
-  //         type: "string",
-  //         format: "binary",
-  //       },
-  //     },
-  //   },
-  // })
+  @ApiConsumes("multipart/form-data")
+  @ApiBody({
+    schema: {
+      type: "object",
+      properties: {
+        file: {
+          type: "string",
+          format: "binary",
+        },
+      },
+    },
+  })
 
-  // @Post('/uploadXlsx')
-  // @UseInterceptors(
-  //   FileInterceptor('file', {
-  //     storage: diskStorage({
-  //       destination: './uploads',
-  //       filename: (req, file, callback) => {
-  //         const uniqueSuffix =
-  //           Date.now() + '-' + Math.round(Math.random() * 1e9);
-  //         const ext = file.originalname.split('.').pop();
-  //         const filename = `${uniqueSuffix}${ext}`;
-  //         callback(null, filename);
-  //       },
-  //     }),
-  //   }),
-  // )
-  // // async handleUpload(@UploadedFile(
-  // //   new ParseFilePipeBuilder()
-  // //     // .addFileTypeValidator({
-  // //     //   // fileType: 'application/pdf'
-  // //     //   // fileType: 'application/octet-stream',
-  // //     // })
-  // //     .build({
-  // //       fileIsRequired: false,
-  // //     }),)
-  // // file?: File,) {
-  // //   console.log('file', file);
-  // //   let workbook = XLSX.readFile(file.path);
-  // //   var sheet_name_list = workbook.SheetNames;
-  // //   var worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], { header: 1, blankrows: false });
-  // //   let Outindex = 0;
-  // //   let resultArray;
+  @Post('/uploadXlsx')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = file.originalname.split('.').pop();
+          const filename = `${uniqueSuffix}${ext}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
+  async handleUpload(@UploadedFile(
+    new ParseFilePipeBuilder()
+      // .addFileTypeValidator({
+      //   // fileType: 'application/pdf'
+      //   // fileType: 'application/octet-stream',
+      // })
+      .build({
+        fileIsRequired: false,
+      }),)
+  file?: File,) {
+    console.log('file', file);
+    let workbook = XLSX.readFile(file.path);
+    var sheet_name_list = workbook.SheetNames;
+    var worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]], { header: 1, blankrows: false });
+    let Outindex = 0;
+    let resultArray;
 
-  // //   for (Outindex = 0; Outindex < worksheet.length; Outindex++) {
-  // //     let newArray = worksheet[Outindex];
-  // //     resultArray = newArray.filter(Boolean)
-  // //     let newCohort: CreateCohortDto = new CreateCohortDto();
-  // //     newCohort.Name = resultArray[1].trim();
-  // //     newCohort.School_Type_ID = resultArray[0];
+    for (Outindex = 0; Outindex < worksheet.length; Outindex++) {
+      let newArray = worksheet[Outindex];
+      resultArray = newArray.filter(Boolean)
+      let newCohort: CreateCohortDto = new CreateCohortDto();
+      newCohort.Name = resultArray[1].trim();
+      newCohort.School_Type_ID = resultArray[0];
 
-  // //     var subject = await this.cohortService.prismaService.subjects.findFirst({
-  // //       where: {
-  // //         Name: resultArray[2].trim()
-  // //       },
-  // //     });
+      var createdCohrot = await this.cohortService.prismaService.cohort.upsert({
+        create: {
+          Created_By: 1,
+          ...newCohort
+        },
+        update: { Name: newCohort.Name },
+        where: { Name: newCohort.Name }
+      });
 
-  // //     await this.cohortService.prismaService.school_type_has_subjects.upsert({
-  // //       update: {
+      var createsSUbjects = await this.cohortService.prismaService.subjects.upsert({
+        create: {
+          Name: resultArray[2].trim(),
+          Created_By: 1,
+        },
+        update: { Name: resultArray[2].trim() },
+        where: { Name: resultArray[2].trim() },
+      });
+      // var subject = await this.cohortService.prismaService.subjects.findFirst({
+      //   where: {
+      //     Name: resultArray[2].trim()
+      //   },
+      // });
 
-  // //         school_type_ID: resultArray[0],
-  // //         subjects_ID: subject.ID
-  // //       }, where: {
-  // //         school_type_ID_subjects_ID: {
-  // //           school_type_ID: resultArray[0],
-  // //           subjects_ID: subject.ID
-  // //         }
-  // //       },
-  // //       create: {
-  // //         school_type_ID: resultArray[0],
-  // //         subjects_ID: subject.ID
-  // //       }
-  // //     });
+      await this.cohortService.prismaService.school_type_has_subjects.upsert({
+        update: {
+          school_type_ID: resultArray[0],
+          subjects_ID: createsSUbjects.ID
+        }, where: {
+          school_type_ID_subjects_ID: {
+            school_type_ID: resultArray[0],
+            subjects_ID: createsSUbjects.ID
+          }
+        },
+        create: {
+          school_type_ID: resultArray[0],
+          subjects_ID: createsSUbjects.ID
+        }
+      });
+      await this.cohortService.prismaService.cohort_has_subjects.upsert({
 
-  // var createdCohrot = await this.cohortService.prismaService.cohort.upsert({
-  //   create: {
-  //     Created_By: 1,
-  //     ...newCohort
-  //   },
-  //   update: { Name: newCohort.Name },
-  //   where: { Name: newCohort.Name }
-  // });
+        create: {
 
-  // var createsSUbjects = await this.cohortService.prismaService.subjects.upsert({
-  //   create: {
-  //     Name: resultArray[2].trim(),
-  //     Created_By: 1,
-  //   },
-  //   update: { Name: resultArray[2].trim() },
-  //   where: { Name: resultArray[2].trim() },
-  // });
+          Cohort_ID: createdCohrot.ID,
+          Subjects_ID: createsSUbjects.ID,
+        },
+        update: {
+          Cohort_ID: createdCohrot.ID,
+          Subjects_ID: createsSUbjects.ID,
 
-  // await this.cohortService.prismaService.cohort_has_subjects.create({
-  //   data: {
-  //     Cohort_ID: createdCohrot.ID,
-  //     Subjects_ID: createsSUbjects.ID,
-  //   }
-  // })
-  //     console.log(`${resultArray[2].trim()} has been added To Subjects! ✅`);
-  //     // console.log(`${newCohort.Name} has been added To Cohort! ✅`);
-  //   }
+        }, where: {
+          Cohort_ID_Subjects_ID: {
+            Cohort_ID: createdCohrot.ID,
+            Subjects_ID: createsSUbjects.ID,
+          }
+        }
+      })
 
-  //   console.log(`All Cohort  are in place! ✅`);
-  //   return 'File upload Done';
-  // }
+      console.log(`${Outindex} has been added To Subjects! ✅`);
+      console.log(`${resultArray[2].trim()} has been added To Subjects! ✅`);
+      console.log(`${resultArray[1].trim()} has been added To Cohort! ✅`);
+    }
+
+    console.log(`All Cohort  are in place! ✅`);
+    return 'File upload Done';
+  }
 }
+
+
